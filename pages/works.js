366275d8964ffc35400c41d11b3363c84e543357
g1st <gintstan@gmail.com>
2018-11-08 21:15:48 +0000

    add isomorphic call for data

--- pages/works.js
+++ pages/works.js
@@ -1,9 +1,40 @@
-const Works = () => {
-  return (
-    <div>
-      <p>This is Works page.</p>
-    </div>
-  );
-};
+import axios from 'axios';
+
+import Layout from '../components/Layout';
+import Gallery from '../components/Gallery/Gallery';
+class Works extends React.Component {
+  static async getInitialProps({ pathname, req }) {
+    if (req) {
+      const { db } = req;
+
+      const data = await db
+        .collection('works')
+        .find()
+        .toArray();
+
+      return { data, pathname, from: 'server' };
+    }
+
+    const works = await axios.get('/api').then(res => {
+      return res.data;
+    });
+
+    return { data: works, pathname, from: 'rest api' };
+  }
+
+  render() {
+    return (
+      <Layout pathname={this.props.pathname}>
+        <div>
+          <p>This is Works page.</p>
+          <p>path: {this.props.pathname}</p>
+          <p>from: {this.props.from}</p>
+          {/* <p>data: {this.props.data}</p> */}
+          <Gallery data={this.props.data} />
+        </div>
+      </Layout>
+    );
+  }
+}
 
 export default Works;
