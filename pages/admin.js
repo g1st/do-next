571af3638d4f2c7b56519618b23dceb7f6901d74
g1st <gintstan@gmail.com>
2018-11-08 21:17:28 +0000

    move AdminForm to separate component

--- pages/admin.js
+++ pages/admin.js
@@ -1,35 +1,12 @@
-import ShoppingIcon from '@material-ui/icons/ShoppingBasket';
-
 import styled from 'styled-components';
 
-const BigFont = styled.p`
-  font-size: 40px;
-  color: grey;
-`;
-
-const Todo = styled.ol`
-  background: lightblue;
-`;
-
-const Item = styled.li`
-  font-size: 38px;
-`;
+import AdminForm from '../components/AdminForm';
 
 const Admin = () => {
   return (
     <div>
       <p>This is Admin page.</p>
-      <ShoppingIcon />
-      <BigFont>Veikia?</BigFont>
-      <Todo>
-        <Item>One</Item>
-        <Item>Two</Item>
-        <Item>Three</Item>
-        <Item>Four</Item>
-        <Item>Four</Item>
-        <Item>Four</Item>
-        <Item>Four</Item>
-      </Todo>
+      <AdminForm />
     </div>
   );
 };
