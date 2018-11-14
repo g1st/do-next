ca7210d7402efdfda6daabb64ad93607dc8bc9a2
g1st <gintstan@gmail.com>
2018-11-14 17:40:34 +0000

    subscribe to CartContainer

--- components/CartDrawer/CartDrawerContent.js
+++ components/CartDrawer/CartDrawerContent.js
@@ -1,28 +1,28 @@
 import React from 'react';
+import { Subscribe } from 'unstated';
 
-// import { CartConsumer } from '../../../../contexts/CartContext';
+import CartContainer from '../../containers/CartContainer';
 
 const CartDrawerContent = () => (
-  <div>
-    <div>this is CartDrawerContent cia vidus:</div>
-    <div>
-      cia reiks gal atskira contexta idetia reiks gal atskira contexta idet ia
-      reiks gal atskira contexta idet ia reiks gal atskira contexta idet
-    </div>
-  </div>
+  <Subscribe to={[CartContainer]}>
+    {cart => (
+      <div>
+        <div>this is CartDrawerContent cia vidus:</div>
+        <ul>
+          {cart.state.selectedItems.map((item, i) => (
+            <li key={i}>{item}</li>
+          ))}
+        </ul>
+        <div>{cart.state.count}</div>
+        <div>
+          <button value={'test'} onClick={el => cart.addItem(el.target.value)}>
+            Add Item
+          </button>
+        </div>
+        <div>hi</div>
+      </div>
+    )}
+  </Subscribe>
 );
-
-// const CartDrawerContent = () => (
-//   <CartConsumer>
-//     {context => {
-//       return (
-//         <div>
-//           <div>this is CartDrawerContent cia vidus: {context.quantity}</div>
-//           <div>ir kaina bus: {context.price}</div>
-//         </div>
-//       );
-//     }}
-//   </CartConsumer>
-// );
 
 export default CartDrawerContent;
