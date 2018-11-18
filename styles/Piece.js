38a78a3515857e8bd67ef4bb5794e4d7312b659f
g1st <gintstan@gmail.com>
2018-11-18 20:30:29 +0000

    add a bit of styling and content

--- styles/Piece.js
+++ styles/Piece.js
@@ -8,16 +8,23 @@ Wrapper.displayName = 'Wrapper';
 
 export const Images = styled.div`
   /* display: flex; */
-  display: inline-block;
-  width: 50%;
+  @media (min-width: 960px) {
+    width: 60%;
+    display: inline-block;
+  }
   background-color: greenyellow;
 `;
 Images.displayName = 'Images';
 
 export const Info = styled.div`
   /* display: flex; */
-  display: inline-block;
-  width: 50%;
+  @media (min-width: 960px) {
+    width: 40%;
+    display: inline-block;
+  }
   background-color: red;
 `;
 Info.displayName = 'Info';
+
+export const Title = styled.h2``;
+Title.displayName = 'Title';
