4d060c656cbd020f68346884ce05fa7f7cb2f8b0
g1st <gintstan@gmail.com>
2018-12-03 16:46:31 +0000

    Add content, still work in progress.

--- components/CartDrawer/CartDrawerContent.js
+++ components/CartDrawer/CartDrawerContent.js
@@ -1,25 +1,86 @@
 import React from 'react';
 import { Subscribe } from 'unstated';
+import Typography from '@material-ui/core/Typography';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+import ClearIcon from '@material-ui/icons/Clear';
+import IconButton from '@material-ui/core/IconButton';
+import Link from 'next/link';
 
 import CartContainer from '../../containers/CartContainer';
+import { CartItems, Thumb, ItemInfo, Totals } from '../../styles/CartDrawer';
 
-const CartDrawerContent = () => (
+const CartDrawerContent = props => (
   <Subscribe to={[CartContainer]}>
     {cart => (
       <div>
-        <div>this is CartDrawerContent cia vidus:</div>
-        <ul>
-          {cart.state.selectedItems.map((item, i) => (
-            <li key={i}>{item}</li>
-          ))}
-        </ul>
-        <div>{cart.state.count}</div>
-        <div>
-          <button value={'test'} onClick={el => cart.addItem(el.target.value)}>
-            Add Item
-          </button>
-        </div>
-        <div>hi</div>
+        {cart.state.selectedItems.length > 0 ? (
+          <CartItems>
+            {cart.state.selectedItems.map((item, i) => (
+              <li key={i}>
+                <Link href={`/piece?id=${item._id}`} as={`/piece/${item._id}`}>
+                  <a
+                    style={{ textDecoration: 'none', height: '48px' }}
+                    target="_self"
+                    onClick={props.closeDrawer}
+                  >
+                    <Thumb src={`/static/uploads/${item.images[0].thumb}`} />
+                  </a>
+                </Link>
+                <ItemInfo>
+                  <Typography
+                    variant="body2"
+                    style={{
+                      overflow: 'hidden',
+                      whiteSpace: 'nowrap',
+                      width: '98px',
+                      textOverflow: 'ellipsis'
+                    }}
+                  >
+                    {item.name}
+                  </Typography>
+                  <Typography variant="body2" style={{ fontWeight: 300 }}>
+                    {item.price}£
+                  </Typography>
+                </ItemInfo>
+                <IconButton
+                  onClick={() => cart.addItem(item)}
+                  color="secondary"
+                  aria-label="Increase quantity"
+                >
+                  <AddIcon fontSize="small" />
+                </IconButton>
+                <Typography variant="body2">{item.quantity}</Typography>
+                <IconButton
+                  disabled={item.quantity > 1 ? false : true}
+                  onClick={() => cart.removeOne(item)}
+                  color="secondary"
+                  aria-label="Decrease quantity"
+                >
+                  <RemoveIcon fontSize="small" />
+                </IconButton>
+                <IconButton
+                  style={{
+                    color: 'grey',
+                    marginLeft: '4px'
+                  }}
+                  aria-label="Remove item"
+                  onClick={() => cart.removeItem(item)}
+                >
+                  <ClearIcon fontSize="small" />
+                </IconButton>
+              </li>
+            ))}
+          </CartItems>
+        ) : (
+          <p>Cart is empty</p>
+        )}
+
+        <Totals>
+          <div>Unique items: {cart.state.count}</div>
+          <div>Total items: {cart.state.totalItems}</div>
+          <div>Total price: {cart.state.totalPrice}£</div>
+        </Totals>
       </div>
     )}
   </Subscribe>
---
+++ styles/CartDrawer.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const CartItems = styled.ul`
+  list-style: none;
+  padding: 0;
+
+  li {
+    display: flex;
+    height: 60px;
+    align-items: center;
+    padding: 8px;
+    background: skyblue;
+    margin-bottom: 5px;
+  }
+`;
+CartItems.displayName = 'CartItems';
+
+export const Thumb = styled.img`
+  width: 48px;
+  height: 48px;
+`;
+Thumb.displayName = 'Thumb';
+
+export const ItemInfo = styled.div`
+  flex-grow: 1;
+  p {
+    margin-left: 8px;
+  }
+`;
+ItemInfo.displayName = 'ItemInfo';
+
+export const Totals = styled.div`
+  float: right;
+  padding-right: 20px;
+`;
+Totals.displayName = 'Totals';
