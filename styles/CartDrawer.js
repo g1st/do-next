54e5137ba35e3a6a02d8cd9527f1767b3ad86011
g1st <gintstan@gmail.com>
2019-01-15 16:10:55 +0000

    Add little table to display order details.

--- styles/CartDrawer.js
+++ styles/CartDrawer.js
@@ -9,7 +9,7 @@ export const CartItems = styled.ul`
     height: 60px;
     align-items: center;
     padding: 8px;
-    background: rgba(0, 0, 0, 0.05);
+    background: rgba(0, 0, 0, 0.03);
     margin-bottom: 5px;
   }
 `;
@@ -34,3 +34,33 @@ export const Totals = styled.div`
   padding-right: 20px;
 `;
 Totals.displayName = 'Totals';
+
+export const Table = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+`;
+Table.displayName = 'Table';
+
+export const TableRow = styled.tr`
+  &:nth-child(even) {
+    background-color: rgba(0, 0, 0, 0.03);
+  }
+`;
+TableRow.displayName = 'TableRow';
+
+export const TableHead = styled.th`
+  padding: 8px;
+  text-align: left;
+  /* border: 1px solid rgba(0,0,0,0.03); */
+`;
+TableHead.displayName = 'TableHead';
+
+export const TableCell = styled.td`
+  /* border: 1px solid rgba(0,0,0,0.03); */
+  padding: 8px;
+  text-align: right;
+`;
+TableCell.displayName = 'TableCell';
+
+export const TableBody = styled.tbody``;
+TableBody.displayName = 'TableBody';
