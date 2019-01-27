3fb214847e6fe7c109d344ce85b7f16eccdf24f5
g1st <gintstan@gmail.com>
2019-01-27 19:38:41 +0000

    Move CartItem to its own component.

--- components/CartDrawer/CartDrawerContent.js
+++ components/CartDrawer/CartDrawerContent.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import Link from 'next/link';
 import Typography from '@material-ui/core/Typography';
-import AddIcon from '@material-ui/icons/Add';
-import RemoveIcon from '@material-ui/icons/Remove';
-import ClearIcon from '@material-ui/icons/Clear';
-import IconButton from '@material-ui/core/IconButton';
 import { connect } from 'react-redux';
 
+import CartItem from './CartItem';
 import { cart } from '../../util/helpers';
 import {
-  increaseQuantity,
-  decreaseQuantity,
-  removeFromCart
-} from '../../store/actions/index_dovile';
-import {
-  CartItems,
-  Thumb,
-  ItemInfo,
   Totals,
   Table,
   TableBody,
@@ -25,216 +13,65 @@ import {
   TableCell
 } from '../../styles/CartDrawer';
 
+const SHIPPING_PRICE = 5; // pounds
+
 const CartDrawerContent = props => {
-  // return (
-  // if (props.buyItNow && cart.state.buyItNow.hasOwnProperty('name')) {
-  //   return (
-  //     <div>
-  //       <CartItems>
-  //         <li key={cart.state.buyItNow._id}>
-  //           <Link
-  //             href={`/piece?id=${cart.state.buyItNow._id}`}
-  //             as={`/piece/${cart.state.buyItNow._id}`}
-  //           >
-  //             <a
-  //               style={{ textDecoration: 'none', height: '48px' }}
-  //               target="_self"
-  //               onClick={props.closeDrawer}
-  //             >
-  //               <Thumb
-  //                 src={`/static/uploads/${
-  //                   cart.state.buyItNow.images[0].thumb
-  //                 }`}
-  //               />
-  //             </a>
-  //           </Link>
-  //           <ItemInfo>
-  //             <Typography
-  //               variant="body2"
-  //               style={{
-  //                 overflow: 'hidden',
-  //                 whiteSpace: 'nowrap',
-  //                 width: '98px',
-  //                 textOverflow: 'ellipsis'
-  //               }}
-  //             >
-  //               {cart.state.buyItNow.name}
-  //             </Typography>
-  //             <Typography variant="body2" style={{ fontWeight: 300 }}>
-  //               £{cart.state.buyItNow.price.toFixed(2)}
-  //             </Typography>
-  //           </ItemInfo>
-  //           <Typography variant="body2">#1</Typography>
-  //         </li>
-  //       </CartItems>
-  //       <Totals />
-  //       <Table>
-  //         <TableBody>
-  //           <TableRow>
-  //             <TableHead>
-  //               <Typography variant="body1">Items</Typography>
-  //             </TableHead>
-  //             <TableCell>
-  //               <Typography variant="body1">{1}</Typography>
-  //             </TableCell>
-  //           </TableRow>
-  //           <TableRow>
-  //             <TableHead>
-  //               <Typography variant="body1">Shipping</Typography>
-  //             </TableHead>
-  //             <TableCell>
-  //               <Typography variant="body1">
-  //                 {/* or some default postage cost from cart i.e. cart.state.postageprice */}
-  //                 {'Free' || cart.state.count}
-  //               </Typography>
-  //             </TableCell>
-  //           </TableRow>
-  //           <TableRow>
-  //             <TableHead>
-  //               <Typography variant="body1">Total</Typography>
-  //             </TableHead>
-  //             <TableCell>
-  //               <Typography variant="body1">
-  //                 £{cart.state.buyItNow.price.toFixed(2)}
-  //               </Typography>
-  //             </TableCell>
-  //           </TableRow>
-  //         </TableBody>
-  //       </Table>
-  //     </div>
-  //   );
-  // } else {
-  // const totalItems = cart =>
-  //   cart.reduce((acc, item) => {
-  //     acc += item.quantity;
-  //     return acc;
-  //   }, 0);
-
-  // const totalPrice = cart =>
-  //   cart.reduce((acc, item) => {
-  //     acc += item.price * item.quantity;
-  //     return acc;
-  //   }, 0);
-
-  return (
-    <div>
-      {props.cart.length > 0 ? (
-        <>
-          <CartItems>
-            {props.cart.map(item => (
-              <li key={item._id}>
-                <Link href={`/piece?id=${item._id}`} as={`/piece/${item._id}`}>
-                  <a
-                    style={{ textDecoration: 'none', height: '48px' }}
-                    target="_self"
-                    onClick={props.closeDrawer}
-                  >
-                    <Thumb src={`/static/uploads/${item.images[0].thumb}`} />
-                  </a>
-                </Link>
-                <ItemInfo>
-                  <Typography
-                    variant="body2"
-                    style={{
-                      overflow: 'hidden',
-                      whiteSpace: 'nowrap',
-                      width: '98px',
-                      textOverflow: 'ellipsis'
-                    }}
-                  >
-                    {item.name}
-                  </Typography>
-                  <Typography variant="body2" style={{ fontWeight: 300 }}>
-                    £{item.price.toFixed(2)}
-                  </Typography>
-                </ItemInfo>
-                <IconButton
-                  disabled={item.quantity > 1 ? false : true}
-                  onClick={() => props.decreaseQuantity(item._id)}
-                  color="secondary"
-                  aria-label="Decrease quantity"
-                >
-                  <RemoveIcon fontSize="small" />
-                </IconButton>
-                <Typography variant="body2">{item.quantity}</Typography>
-                <IconButton
-                  onClick={() => props.increaseQuantity(item._id)}
-                  color="secondary"
-                  aria-label="Increase quantity"
-                >
-                  <AddIcon fontSize="small" />
-                </IconButton>
-                <IconButton
-                  style={{ color: 'rgba(0, 0, 0, 0.26)', marginLeft: '4px' }}
-                  aria-label="Remove item"
-                  onClick={() =>
-                    window.confirm(
-                      'Are you sure you want to remove this item?'
-                    ) && props.removeFromCart(item._id)
-                  }
-                >
-                  <ClearIcon fontSize="small" />
-                </IconButton>
-              </li>
-            ))}
-          </CartItems>
-
-          <Totals />
-          <Table>
-            <TableBody>
-              <TableRow>
-                <TableHead>
-                  <Typography variant="body1">Items</Typography>
-                </TableHead>
-                <TableCell>
-                  <Typography variant="body1">
-                    {cart.totalItems(props.cart)}
-                  </Typography>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableHead>
-                  <Typography variant="body1">Shipping</Typography>
-                </TableHead>
-                <TableCell>
-                  <Typography variant="body1">{'£8' || 'Free'}</Typography>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableHead>
-                  <Typography variant="body1">Total</Typography>
-                </TableHead>
-                <TableCell>
-                  <Typography variant="body1">
-                    £{cart.totalPrice(props.cart).toFixed(2)}
-                  </Typography>
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </>
-      ) : (
-        <p>Cart is empty</p>
-      )}
-    </div>
-  );
+  if (props.cart.length > 0 || props.buyItNow) {
+    return (
+      <div>
+        <CartItem
+          data={props.buyItNow ? [props.buyItNowItem] : props.cart}
+          buyItNow={props.buyItNow}
+        />
+        <Totals />
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableHead>
+                <Typography variant="body1">Items</Typography>
+              </TableHead>
+              <TableCell>
+                <Typography variant="body1">
+                  {props.buyItNow
+                    ? props.buyItNowItem.quantity
+                    : cart.totalItems(props.cart)}
+                </Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableHead>
+                <Typography variant="body1">Shipping</Typography>
+              </TableHead>
+              <TableCell>
+                <Typography variant="body1">
+                  {`£${SHIPPING_PRICE}` || 'Free'}
+                </Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableHead>
+                <Typography variant="body1">Total</Typography>
+              </TableHead>
+              <TableCell>
+                <Typography variant="body1">
+                  £
+                  {props.buyItNow
+                    ? cart.totalPrice([props.buyItNowItem]).toFixed(2)
+                    : cart.totalPrice(props.cart).toFixed(2)}
+                </Typography>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+    );
+  }
+  return <p>Cart is empty</p>;
 };
 
 const mapStateToProps = state => ({
-  cart: state.cart
+  cart: state.cart,
+  buyItNowItem: state.buyItNow
 });
 
-const mapDispatchToProps = dispatch => {
-  return {
-    increaseQuantity: id => dispatch(increaseQuantity(id)),
-    decreaseQuantity: id => dispatch(decreaseQuantity(id)),
-    removeFromCart: id => dispatch(removeFromCart(id))
-  };
-};
-
-export default React.memo(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(CartDrawerContent)
-);
+export default React.memo(connect(mapStateToProps)(CartDrawerContent));
---
+++ components/CartDrawer/CartItem.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import Link from 'next/link';
+import Typography from '@material-ui/core/Typography';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+import ClearIcon from '@material-ui/icons/Clear';
+import IconButton from '@material-ui/core/IconButton';
+import { connect } from 'react-redux';
+
+import {
+  increaseQuantity,
+  decreaseQuantity,
+  removeFromCart,
+  buyItNowIncreaseQuantity,
+  buyItNowDecreaseQuantity,
+  clearBuyItNow
+} from '../../store/actions';
+import { CartItems, Thumb, ItemInfo } from '../../styles/CartDrawer';
+
+const CartItem = props => {
+  const handleRemove = id => {
+    props.buyItNow ? props.clearBuyItNow() : props.removeFromCart(id);
+  };
+
+  return (
+    <CartItems>
+      {props.data.map(item => (
+        <li key={item._id}>
+          <Link href={`/piece?id=${item._id}`} as={`/piece/${item._id}`}>
+            <a
+              style={{ textDecoration: 'none', height: '48px' }}
+              target="_self"
+              onClick={props.closeDrawer}
+            >
+              <Thumb src={`/static/uploads/${item.images[0].thumb}`} />
+            </a>
+          </Link>
+          <ItemInfo>
+            <Typography
+              variant="body2"
+              style={{
+                overflow: 'hidden',
+                whiteSpace: 'nowrap',
+                width: '98px',
+                textOverflow: 'ellipsis'
+              }}
+            >
+              {item.name}
+            </Typography>
+            <Typography variant="body2" style={{ fontWeight: 300 }}>
+              £{item.price.toFixed(2)}
+            </Typography>
+          </ItemInfo>
+          <IconButton
+            disabled={item.quantity > 1 ? false : true}
+            onClick={
+              props.buyItNow
+                ? () => props.buyItNowDecreaseQuantity()
+                : () => props.decreaseQuantity(item._id)
+            }
+            color="secondary"
+            aria-label="Decrease quantity"
+          >
+            <RemoveIcon fontSize="small" />
+          </IconButton>
+          <Typography variant="body2">{item.quantity}</Typography>
+          <IconButton
+            onClick={
+              props.buyItNow
+                ? () => props.buyItNowIncreaseQuantity()
+                : () => props.increaseQuantity(item._id)
+            }
+            color="secondary"
+            aria-label="Increase quantity"
+          >
+            <AddIcon fontSize="small" />
+          </IconButton>
+          <IconButton
+            style={{ color: 'rgba(0, 0, 0, 0.26)', marginLeft: '4px' }}
+            aria-label="Remove item"
+            onClick={() =>
+              window.confirm('Are you sure you want to remove this item?') &&
+              handleRemove(item._id)
+            }
+          >
+            <ClearIcon fontSize="small" />
+          </IconButton>
+        </li>
+      ))}
+    </CartItems>
+  );
+};
+
+const mapStateToProps = state => ({
+  cart: state.cart,
+  buyItNowItem: state.buyItNow
+});
+
+const mapDispatchToProps = dispatch => {
+  return {
+    increaseQuantity: id => dispatch(increaseQuantity(id)),
+    decreaseQuantity: id => dispatch(decreaseQuantity(id)),
+    removeFromCart: id => dispatch(removeFromCart(id)),
+    buyItNowIncreaseQuantity: () => dispatch(buyItNowIncreaseQuantity()),
+    buyItNowDecreaseQuantity: () => dispatch(buyItNowDecreaseQuantity()),
+    clearBuyItNow: () => dispatch(clearBuyItNow())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CartItem);
