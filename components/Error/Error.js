d2ed48403140d69572479e9af031d22bd774e16f
g1st <gintstan@gmail.com>
2019-02-10 12:59:44 +0000

    Revert back to default error color.

--- components/Error/Error.js
+++ components/Error/Error.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
 import FormHelperText from '@material-ui/core/FormHelperText';
 
-const Error = props => (
-  // Prefer error message not to be colored red
-  <FormHelperText error={false}>{props.children}</FormHelperText>
-);
+const Error = props => <FormHelperText>{props.children}</FormHelperText>;
 
 export default Error;
