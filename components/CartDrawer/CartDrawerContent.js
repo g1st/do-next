0e7e92237bdc3df7c3206f4297c32648c6dfc98d
g1st <gintstan@gmail.com>
2019-02-21 21:16:34 +0000

    Add a bit of content to empty state of Cart drawer.

--- components/CartDrawer/CartDrawerContent.js
+++ components/CartDrawer/CartDrawerContent.js
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import Router from 'next/router';
 
 import CartItem from './CartItem';
 import { cart } from '../../util/helpers';
@@ -14,10 +17,41 @@ import {
   TableCell
 } from '../../styles/CartDrawer';
 
+const styles = {
+  button: {
+    margin: '0 auto',
+    display: 'flex',
+    marginTop: '32px'
+  }
+};
+
 const CartDrawerContent = props => {
+  const buttonClickHandler = () => {
+    Router.push('/works');
+    props.closeDrawer();
+  };
+
+  const { classes } = props;
+
+  let content = (
+    <>
+      <Typography variant="body1" align="center">
+        Cart is currently empty.
+      </Typography>
+      <Button
+        className={classes.button}
+        size="medium"
+        variant="contained"
+        color="secondary"
+        onClick={() => buttonClickHandler()}
+      >
+        SHOP
+      </Button>
+    </>
+  );
   if (props.cart.length > 0 || props.buyItNow) {
-    return (
-      <div>
+    content = (
+      <>
         <CartItem
           data={props.buyItNow ? [props.buyItNowItem] : props.cart}
           buyItNow={props.buyItNow}
@@ -68,10 +102,17 @@ const CartDrawerContent = props => {
             </TableRow>
           </TableBody>
         </Table>
-      </div>
+      </>
     );
   }
-  return <p>Cart is empty</p>;
+  return (
+    <>
+      <Typography variant="h5" align="center" gutterBottom>
+        Your Cart
+      </Typography>
+      {content}
+    </>
+  );
 };
 
 const mapStateToProps = state => ({
@@ -84,4 +125,6 @@ CartDrawerContent.propTypes = {
   buyItNow: PropTypes.bool
 };
 
-export default React.memo(connect(mapStateToProps)(CartDrawerContent));
+export default React.memo(
+  connect(mapStateToProps)(withStyles(styles)(CartDrawerContent))
+);
