66a0e486f12ce4eb7deb9bdc62d66e052ab5c702
g1st <gintstan@gmail.com>
2019-03-13 21:22:59 +0000

    Remove files from disk.

--- api/api.js
+++ api/api.js
@@ -47,22 +47,21 @@ module.exports = (db, upload) => {
     })
   );
 
-  router.delete(
-    '/delete',
-    wrapAsync(async function(req) {
-      const id = req.query.id;
+  router.delete('/delete', async function(req, res) {
+    try {
+      const id = req.query._id;
+      const works = await Works.findOneAndRemove({ _id: id });
 
-      // const works = await Works.findByIdAndDelete(id, (err, data) => {
-      const works = await Works.findOneAndRemove({ id }, (err, data) => {
-        if (err) return console.error(err);
-      });
-      console.log('deleted works', works);
+      // removeImagesFromDisk needs thumb as starter
+      const imagesToRemove = works.images.map(img => img.thumb);
+      // remove photos from disk
+      serverUtils.removeImagesFromDisk(imagesToRemove);
 
-      return works;
-
-      // todo: delete photos from disk
-    })
-  );
+      res.json({ deletedItem: works.name });
+    } catch (err) {
+      console.errlogor(err);
+    }
+  });
 
   router.patch('/update', upload.array('photos[]', 10), async (req, res) => {
     const imageSizes = { big: 900, medium: 300, thumb: 92 };
