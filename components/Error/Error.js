15c23632ffdf4c06bab4c68156f76bbd317b2a65
g1st <gintstan@gmail.com>
2019-03-18 16:07:26 +0000

    Give it error colouring.

--- components/Error/Error.js
+++ components/Error/Error.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import FormHelperText from '@material-ui/core/FormHelperText';
 
-const Error = props => <FormHelperText>{props.children}</FormHelperText>;
+const Error = props => <FormHelperText error>{props.children}</FormHelperText>;
 
 export default Error;
