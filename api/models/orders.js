d6d606c527374dadf192a6415685f16b4c0d1042
g1st <gintstan@gmail.com>
2019-03-26 10:24:19 +0000

    Save client ref instead of raw data.

--- api/models/orders.js
+++ api/models/orders.js
@@ -2,15 +2,11 @@ const mongoose = require('mongoose');
 
 const ordersSchema = new mongoose.Schema(
   {
-    first_name: { type: String, trim: true },
-    last_name: { type: String, trim: true },
-    email: { type: String, trim: true },
-    phone: { type: String, trim: true },
-    address1: { type: String, trim: true },
-    address2: { type: String, trim: true },
-    city: { type: String, trim: true },
-    country: { type: String, trim: true },
-    client_ip: { type: String, trim: true },
+    client: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Client',
+      required: true
+    },
     transaction_id: { type: String, trim: true },
     receipt: { type: String, trim: true },
     amount_paid: { type: Number },
