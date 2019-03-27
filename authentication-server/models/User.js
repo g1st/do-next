6fdbee218a68e326033cc1ed3daf56063647556c
Michal Weizman <m.weizman@gmail.com>
2019-03-27 08:49:36 +0000

    Added required field to email and password in User

--- authentication-server/models/User.js
+++ authentication-server/models/User.js
@@ -9,9 +9,13 @@ const userSchema = new Schema({
   email: {
     type: String,
     unique: true,
-    lowercase: true
+    lowercase: true,
+    required: true
   },
-  password: String
+  password: {
+    type: String,
+    required: true
+  }
 });
 
 // encrypt password before saving a model
