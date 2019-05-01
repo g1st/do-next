713f2ec896db56d3087f561b46f83e9398fc35ca
g1st <gintstan@gmail.com>
2019-05-01 15:05:59 +0100

    style terms-and-conditions

--- styles/TermsAndConditions.js
+++ styles/TermsAndConditions.js
@@ -1,43 +1,32 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  margin: 0 10px;
-
-  @media (min-width: 960px) {
-    margin: 0;
-    display: grid;
-    grid-gap: 80px;
-    grid-template-columns: 1fr 1fr;
-  }
+  margin-top: -20px;
+  width: 100%;
 `;
 Wrapper.displayName = 'Wrapper';
 
 export const Text = styled.div`
-  @media (min-width: 960px) {
-    grid-column-start: 1;
-    grid-row-start: 1;
-  }
+  max-width: 700px;
+  margin: 0 auto;
 `;
 Text.displayName = 'Text';
 
 export const Figure = styled.figure`
-  margin-left: 20px;
-  margin-right: 20px;
-  @media (min-width: 960px) {
-    margin-left: 40px;
-    margin-right: 40px;
-  }
+  display: inline-block;
+  margin: 0;
 `;
 Figure.displayName = 'Figure';
+
+export const ImageWrapper = styled.div`
+  width: 100%;
+  text-align: center;
+`;
+ImageWrapper.displayName = 'ImageWrapper';
 
 export const Image = styled.img`
   max-width: 100%;
   max-height: 100%;
-  border: 1px solid rgba(0, 0, 0, 0.1);
-
-  @media (min-width: 960px) {
-    margin-top: 20px;
-    grid-column-start: 2;
-  }
+  margin: 0 auto;
 `;
 Image.displayName = 'Image';
