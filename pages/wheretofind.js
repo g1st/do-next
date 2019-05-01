b0d38f109ee0ba35b17df4185e541d407cf9b592
g1st <gintstan@gmail.com>
2019-05-01 15:02:40 +0100

    style wheretofind page

--- pages/wheretofind.js
+++ pages/wheretofind.js
@@ -1,30 +1,25 @@
 import React from 'react';
-import { Typography, List, ListItem } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { Typography } from '@material-ui/core';
 
 import Layout from '../components/Layout';
 import {
   Wrapper,
   Stockist,
+  UpcomingEvents,
   Events,
+  LatestEvents,
   ImageWrapper,
-  Table,
-  Row,
-  Data
+  Image
 } from '../styles/WhereToFind';
-import { AnchorLink } from '../styles/Shared';
+import { AnchorLink, Table, Row, Data } from '../styles/Shared';
 
 const WhereToFind = ({ pathname, collections }) => (
   <Layout pathname={pathname} collections={collections}>
     <Wrapper>
       <Stockist>
         <ImageWrapper>
-          <img
-            src="/static/images/Dovile Jewellery-6.JPG"
-            alt="dovile jewellery"
-            style={{
-              width: '100%'
-            }}
-          />
+          <Image src="/static/images/ef-13.JPG" alt="dovile jewellery" />
         </ImageWrapper>
         <Typography variant="h5" gutterBottom>
           STOCKIST
@@ -80,233 +75,238 @@ const WhereToFind = ({ pathname, collections }) => (
         </Typography>
       </Stockist>
       <Events>
-        <ImageWrapper>
-          <img
-            src="/static/images/Dovile Jewellery-7.JPG"
-            alt="dovile jewellery"
-            style={{
-              width: '100%'
-            }}
-          />
-        </ImageWrapper>
-        <Typography variant="h5" gutterBottom>
-          UPCOMING EVENTS
-        </Typography>
-        <Table>
-          <tbody>
-            <Row>
-              <Data>
-                {' '}
-                <Typography variant="body1">3-27 Aug</Typography>
-              </Data>
-              <Data>
-                <AnchorLink
-                  href="http://www.dazzle-exhibitions.co.uk/Exhibitions/Exhibition.aspx?pageid=1674"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+        <UpcomingEvents>
+          <ImageWrapper>
+            <Image src="/static/images/gf-14.JPG" alt="dovile jewellery" />
+          </ImageWrapper>
+          <Typography variant="h5" gutterBottom>
+            UPCOMING EVENTS
+          </Typography>
+          <Table>
+            <tbody>
+              <Row>
+                <Data>
+                  {' '}
+                  <Typography variant="body1">3-27 Aug</Typography>
+                </Data>
+                <Data>
+                  <AnchorLink
+                    href="http://www.dazzle-exhibitions.co.uk/Exhibitions/Exhibition.aspx?pageid=1674"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Typography variant="body1" gutterBottom>
+                      DAZZLE@DOVECOT, Edinburgh, UK
+                    </Typography>
+                  </AnchorLink>
+                </Data>
+              </Row>
+              <Row>
+                <Data>
+                  {' '}
+                  <Typography variant="body1">3-27 Aug</Typography>
+                </Data>
+                <Data>
+                  <AnchorLink
+                    href="http://www.dazzle-exhibitions.co.uk/Exhibitions/Exhibition.aspx?pageid=1674"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Typography variant="body1" gutterBottom>
+                      DAZZLE@DOVECOT, Edinburgh, UK
+                    </Typography>
+                  </AnchorLink>
+                </Data>
+              </Row>
+              <Row>
+                <Data>
+                  {' '}
+                  <Typography variant="body1">3-27 Aug</Typography>
+                </Data>
+                <Data>
+                  <AnchorLink
+                    href="http://www.dazzle-exhibitions.co.uk/Exhibitions/Exhibition.aspx?pageid=1674"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Typography variant="body1" gutterBottom>
+                      DAZZLE@DOVECOT, Edinburgh, UK
+                    </Typography>
+                  </AnchorLink>
+                </Data>
+              </Row>
+            </tbody>
+          </Table>
+        </UpcomingEvents>
+        <LatestEvents>
+          <Typography variant="h5" gutterBottom>
+            LATEST EVENTS
+          </Typography>
+          <Table>
+            <tbody>
+              <Row>
+                <Data>
+                  {' '}
+                  <Typography variant="body1">2018 Jun</Typography>
+                </Data>
+                <Data>
                   <Typography variant="body1" gutterBottom>
-                    DAZZLE@DOVECOT, Edinburgh, UK
+                    THE CONTEMPORARY CRAFT FESTIVAL, Bovey Tracey, Devon, UK
                   </Typography>
-                </AnchorLink>
-              </Data>
-            </Row>
-            <Row>
-              <Data>
-                {' '}
-                <Typography variant="body1">3-27 Aug</Typography>
-              </Data>
-              <Data>
-                <AnchorLink
-                  href="http://www.dazzle-exhibitions.co.uk/Exhibitions/Exhibition.aspx?pageid=1674"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+                </Data>
+              </Row>
+              <Row>
+                <Data>
+                  {' '}
+                  <Typography variant="body1">2018 Mar</Typography>
+                </Data>
+                <Data>
                   <Typography variant="body1" gutterBottom>
-                    DAZZLE@DOVECOT, Edinburgh, UK
+                    AMBER TRIP, XV International Baltic Jewellery show, Litexpo,
+                    Vilnius, Lithuania
                   </Typography>
-                </AnchorLink>
-              </Data>
-            </Row>
-            <Row>
-              <Data>
-                {' '}
-                <Typography variant="body1">3-27 Aug</Typography>
-              </Data>
-              <Data>
-                <AnchorLink
-                  href="http://www.dazzle-exhibitions.co.uk/Exhibitions/Exhibition.aspx?pageid=1674"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+                </Data>
+              </Row>
+              <Row>
+                <Data>
+                  {' '}
+                  <Typography variant="body1">2017 Nov</Typography>
+                </Data>
+                <Data>
                   <Typography variant="body1" gutterBottom>
-                    DAZZLE@DOVECOT, Edinburgh, UK
+                    MAKING IT NOW, group exhibition curated by Gregory Parsons,
+                    Ruthin Craft Centre, Ruthin, Wales, UK
                   </Typography>
-                </AnchorLink>
-              </Data>
-            </Row>
-          </tbody>
-        </Table>
-        <Typography variant="h5" gutterBottom>
-          LATEST EVENTS
-        </Typography>
-        <Table>
-          <tbody>
-            <Row>
-              <Data>
-                {' '}
-                <Typography variant="body1">2018 Jun</Typography>
-              </Data>
-              <Data>
-                <Typography variant="body1" gutterBottom>
-                  THE CONTEMPORARY CRAFT FESTIVAL, Bovey Tracey, Devon, UK
-                </Typography>
-              </Data>
-            </Row>
-            <Row>
-              <Data>
-                {' '}
-                <Typography variant="body1">2018 Mar</Typography>
-              </Data>
-              <Data>
-                <Typography variant="body1" gutterBottom>
-                  AMBER TRIP, XV International Baltic Jewellery show, Litexpo,
-                  Vilnius, Lithuania
-                </Typography>
-              </Data>
-            </Row>
-            <Row>
-              <Data>
-                {' '}
-                <Typography variant="body1">2017 Nov</Typography>
-              </Data>
-              <Data>
-                <Typography variant="body1" gutterBottom>
-                  MAKING IT NOW, group exhibition curated by Gregory Parsons,
-                  Ruthin Craft Centre, Ruthin, Wales, UK
-                </Typography>
-              </Data>
-            </Row>
-            <Row>
-              <Data>
-                {' '}
-                <Typography variant="body1">2017 Nov</Typography>
-              </Data>
-              <Data>
-                <Typography variant="body1" gutterBottom>
-                  PRESENT, group exhibition, Studio Fusion Gallery, Oxo Tower
-                  Wharf, London, UK
-                </Typography>
-              </Data>
-            </Row>
-            <Row>
-              <Data>
-                {' '}
-                <Typography variant="body1">2017 Nov</Typography>
-              </Data>
-              <Data>
-                <Typography variant="body1" gutterBottom>
-                  DAZZLE LONDON, group exhibition, Oxo Tower Wharf, London, UK
-                </Typography>
-              </Data>
-            </Row>
-            <Row>
-              <Data>
-                {' '}
-                <Typography variant="body1">2017 Nov</Typography>
-              </Data>
-              <Data>
-                <Typography variant="body1" gutterBottom>
-                  MADE BY HAND CARDIFF, contemporary craft fair, Cardiff, UK
-                </Typography>
-              </Data>
-            </Row>
-            <Row>
-              <Data>
-                {' '}
-                <Typography variant="body1">2017 Sep</Typography>
-              </Data>
-              <Data>
-                <Typography variant="body1" gutterBottom>
-                  LUMINATES, group exhibition, Mint Shop, London, UK
-                </Typography>
-              </Data>
-            </Row>
-            <Row>
-              <Data>
-                {' '}
-                <Typography variant="body1">2017 Jul</Typography>
-              </Data>
-              <Data>
-                <Typography variant="body1" gutterBottom>
-                  NEW DESIGNERS, ONE YEAR ON, Business Design Centre, London, UK
-                </Typography>
-              </Data>
-            </Row>
-            <Row>
-              <Data>
-                {' '}
-                <Typography variant="body1">2017 Jun</Typography>
-              </Data>
-              <Data>
-                <Typography variant="body1" gutterBottom>
-                  THE CONTEMPORARY CRAFT FESTIVAL, Bovey Tracey, Devon, UK
-                </Typography>
-              </Data>
-            </Row>
-            <Row>
-              <Data>
-                {' '}
-                <Typography variant="body1">2017 Mar</Typography>
-              </Data>
-              <Data>
-                <Typography variant="body1" gutterBottom>
-                  AMBER TRIP, Art Jewellery Competition show & jewellery show
-                  Vilnius, Lithuania.
-                </Typography>
-              </Data>
-            </Row>
-            <Row>
-              <Data>
-                {' '}
-                <Typography variant="body1">2017 Jan</Typography>
-              </Data>
-              <Data>
-                <Typography variant="body1" gutterBottom>
-                  NATURE MORTE: Contemporary artists reinvigorate the Still-Life
-                  tradition, group exhibition, Wroclaw National Museum. Wroclav,
-                  Poland.
-                </Typography>
-              </Data>
-            </Row>
-            <Row>
-              <Data>
-                {' '}
-                <Typography variant="body1">2016 Jan</Typography>
-              </Data>
-              <Data>
-                <Typography variant="body1" gutterBottom>
-                  THE EYE/HOME, group exhibition, Magan gallery, London, UK
-                </Typography>
-              </Data>
-            </Row>
-            <Row>
-              <Data>
-                {' '}
-                <Typography variant="body1">2015 Sep</Typography>
-              </Data>
-              <Data>
-                <Typography variant="body1" gutterBottom>
-                  THE EYE, Contemporary Jewellery Symposium, Telsiai, Lithuania
-                </Typography>
-              </Data>
-            </Row>
-          </tbody>
-        </Table>
+                </Data>
+              </Row>
+              <Row>
+                <Data>
+                  {' '}
+                  <Typography variant="body1">2017 Nov</Typography>
+                </Data>
+                <Data>
+                  <Typography variant="body1" gutterBottom>
+                    PRESENT, group exhibition, Studio Fusion Gallery, Oxo Tower
+                    Wharf, London, UK
+                  </Typography>
+                </Data>
+              </Row>
+              <Row>
+                <Data>
+                  {' '}
+                  <Typography variant="body1">2017 Nov</Typography>
+                </Data>
+                <Data>
+                  <Typography variant="body1" gutterBottom>
+                    DAZZLE LONDON, group exhibition, Oxo Tower Wharf, London, UK
+                  </Typography>
+                </Data>
+              </Row>
+              <Row>
+                <Data>
+                  {' '}
+                  <Typography variant="body1">2017 Nov</Typography>
+                </Data>
+                <Data>
+                  <Typography variant="body1" gutterBottom>
+                    MADE BY HAND CARDIFF, contemporary craft fair, Cardiff, UK
+                  </Typography>
+                </Data>
+              </Row>
+              <Row>
+                <Data>
+                  {' '}
+                  <Typography variant="body1">2017 Sep</Typography>
+                </Data>
+                <Data>
+                  <Typography variant="body1" gutterBottom>
+                    LUMINATES, group exhibition, Mint Shop, London, UK
+                  </Typography>
+                </Data>
+              </Row>
+              <Row>
+                <Data>
+                  {' '}
+                  <Typography variant="body1">2017 Jul</Typography>
+                </Data>
+                <Data>
+                  <Typography variant="body1" gutterBottom>
+                    NEW DESIGNERS, ONE YEAR ON, Business Design Centre, London,
+                    UK
+                  </Typography>
+                </Data>
+              </Row>
+              <Row>
+                <Data>
+                  {' '}
+                  <Typography variant="body1">2017 Jun</Typography>
+                </Data>
+                <Data>
+                  <Typography variant="body1" gutterBottom>
+                    THE CONTEMPORARY CRAFT FESTIVAL, Bovey Tracey, Devon, UK
+                  </Typography>
+                </Data>
+              </Row>
+              <Row>
+                <Data>
+                  {' '}
+                  <Typography variant="body1">2017 Mar</Typography>
+                </Data>
+                <Data>
+                  <Typography variant="body1" gutterBottom>
+                    AMBER TRIP, Art Jewellery Competition show & jewellery show
+                    Vilnius, Lithuania.
+                  </Typography>
+                </Data>
+              </Row>
+              <Row>
+                <Data>
+                  {' '}
+                  <Typography variant="body1">2017 Jan</Typography>
+                </Data>
+                <Data>
+                  <Typography variant="body1" gutterBottom>
+                    NATURE MORTE: Contemporary artists reinvigorate the
+                    Still-Life tradition, group exhibition, Wroclaw National
+                    Museum. Wroclav, Poland.
+                  </Typography>
+                </Data>
+              </Row>
+              <Row>
+                <Data>
+                  {' '}
+                  <Typography variant="body1">2016 Jan</Typography>
+                </Data>
+                <Data>
+                  <Typography variant="body1" gutterBottom>
+                    THE EYE/HOME, group exhibition, Magan gallery, London, UK
+                  </Typography>
+                </Data>
+              </Row>
+              <Row>
+                <Data>
+                  {' '}
+                  <Typography variant="body1">2015 Sep</Typography>
+                </Data>
+                <Data>
+                  <Typography variant="body1" gutterBottom>
+                    THE EYE, Contemporary Jewellery Symposium, Telsiai,
+                    Lithuania
+                  </Typography>
+                </Data>
+              </Row>
+            </tbody>
+          </Table>
+        </LatestEvents>
       </Events>
     </Wrapper>
   </Layout>
 );
+
+WhereToFind.propTypes = {
+  collections: PropTypes.arrayOf(PropTypes.string),
+  pathname: PropTypes.oneOfType([PropTypes.string, PropTypes.bool])
+};
 
 WhereToFind.getInitialProps = async ({ pathname }) => ({ pathname });
 
--- styles/WhereToFind.js
+++ styles/WhereToFind.js
@@ -6,20 +6,18 @@ export const Wrapper = styled.div`
   margin: 0 10px;
 
   @media (min-width: 960px) {
-    width: 100%;
     flex-flow: row;
-    justify-content: space-around;
+    justify-content: space-between;
     margin-top: 30px;
+    width: 100%;
   }
 `;
 Wrapper.displayName = 'Wrapper';
 
 export const Stockist = styled.div`
   @media (min-width: 960px) {
-    display: flex;
-    flex-direction: column;
-    max-width: 480px;
-    padding-right: 25px;
+    width: 50%;
+    padding-right: 30px;
   }
 `;
 Stockist.displayName = 'Stockist';
@@ -28,13 +26,19 @@ export const Events = styled.div`
   margin-bottom: 50px;
   @media (min-width: 960px) {
     margin-bottom: 0;
-    display: flex;
-    flex-direction: column;
-    max-width: 480px;
-    padding-left: 25px;
+    width: 50%;
+    padding-left: 30px;
   }
 `;
 Events.displayName = 'Events';
+
+export const UpcomingEvents = styled.div`
+  margin-bottom: 3rem;
+`;
+UpcomingEvents.displayName = 'UpcomingEvents';
+
+export const LatestEvents = styled.div``;
+LatestEvents.displayName = 'LatestEvents';
 
 export const ImageWrapper = styled.div`
   max-width: 480px;
@@ -42,28 +46,13 @@ export const ImageWrapper = styled.div`
 
   @media (min-width: 960px) {
     max-width: 480px;
-    margin: 0 auto;
-    margin-bottom: 30px;
+    margin: 0 auto 60px auto;
   }
 `;
 ImageWrapper.displayName = 'ImageWrapper';
 
-export const Table = styled.table`
-  margin-bottom: 14px;
-  vertical-align: top;
+export const Image = styled.img`
+  width: 100%;
+  border-radius: 3px;
 `;
-Table.displayName = 'Table';
-
-export const Row = styled.tr`
-  vertical-align: top;
-  & td:first-child {
-    padding-right: 8px;
-    white-space: nowrap;
-  }
-`;
-Row.displayName = 'Row';
-
-export const Data = styled.td`
-  max-width: 600px;
-`;
-Data.displayName = 'Data';
+Image.displayName = 'Image';
