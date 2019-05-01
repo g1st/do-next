8395834ef665e1db266f578dffcdf4fa1daa42a6
g1st <gintstan@gmail.com>
2019-05-01 09:25:01 +0100

    Lighter shadow on hover

--- components/ItemCard/ItemCard.js
+++ components/ItemCard/ItemCard.js
@@ -14,7 +14,7 @@ const styles = theme => ({
     '&:hover': {
       cursor: 'pointer',
       boxShadow:
-        '-1px 2px 6px 0px rgba(0,0,0,0.2), 0px 2px 1px 0px rgba(0,0,0,0.14), 2px 2px 2px 0px rgba(0,0,0,0.12)'
+        '0px 1px 3px 0px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 2px 1px -1px rgba(0,0,0,0.12)'
     },
     '@media(min-width: 460px)': {
       maxWidth: '300px'
