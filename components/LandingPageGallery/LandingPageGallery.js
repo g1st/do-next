74e09bd53be79cfb470f5ea96481a19312fbb643
g1st <gintstan@gmail.com>
2019-05-02 21:35:51 +0100

    add descriptions

--- components/LandingPageGallery/LandingPageGallery.js
+++ components/LandingPageGallery/LandingPageGallery.js
@@ -6,7 +6,6 @@ import Modal from '@material-ui/core/Modal';
 
 import {
   Wrapper,
-  Headline,
   Content,
   Input,
   ModalImage,
@@ -46,8 +45,25 @@ const styles = theme => ({
     transform: 'translate(-50%, -50%)',
     width: '80vw',
 
-    '@media (min-width: 800px)': {
-      maxWidth: '750px'
+    '@media (min-width: 600px)': {
+      maxWidth: '814px',
+      maxHeight: '90vh'
+    }
+  },
+  headline: {
+    textTransform: 'uppercase',
+    letterSpacing: '4px',
+    marginTop: '40px',
+    fontSize: '1rem',
+    '@media (min-width: 960px)': {
+      fontSize: '1.5rem'
+    }
+  },
+  author: {
+    fontSize: '1rem',
+    marginTop: '0.6rem',
+    '@media (min-width: 960px)': {
+      fontSize: '1.3rem'
     }
   }
 });
@@ -55,23 +71,26 @@ const styles = theme => ({
 class LandingPageGallery extends React.Component {
   state = { open: false };
 
-  onImageClick = ({ target: { src } }) => {
-    this.setState({ open: true, src });
+  // onImageClick = ({ target: { src } }, text) => {
+  //   this.setState({ open: true, src, text });
+  // };
+  onImageClick = ({ target: { src } }, figcaption) => {
+    this.setState({ open: true, src, figcaption });
   };
 
   handleClose = () => {
     this.setState({ open: false });
   };
 
-  handleKeyDown = ({ key, target: { src } }) => {
+  handleKeyDown = ({ key, target: { src } }, figcaption) => {
     if (key === 'Enter' || key === ' ') {
-      this.setState({ open: true, src });
+      this.setState({ open: true, src, figcaption });
     }
   };
 
   render() {
     const { classes } = this.props;
-    const { open, src } = this.state;
+    const { open, src, figcaption } = this.state;
 
     return (
       <Wrapper>
@@ -82,14 +101,13 @@ class LandingPageGallery extends React.Component {
           onClose={this.handleClose}
         >
           <figure className={classes.paper}>
-            <ModalImage src={src} alt="" />
+            <ModalImage
+              src={src}
+              alt="Presentational Dovile Jewellery art piece"
+            />
             <Figcaption>
-              <Typography
-                variant="subtitle1"
-                id="jewellery-piece-description"
-                className={classes.figcaption}
-              >
-                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              <Typography variant="subtitle1" id="jewellery-piece-description">
+                {figcaption}
               </Typography>
             </Figcaption>
           </figure>
@@ -100,19 +118,39 @@ class LandingPageGallery extends React.Component {
               <Image>
                 <Input
                   type="image"
-                  onClick={this.onImageClick}
+                  onClick={e =>
+                    this.onImageClick(
+                      e,
+                      'FLOW n°B8 and n°B10 | brooches | Baltic amber, ebony, steel'
+                    )
+                  }
                   src="/static/images/Dovile Jewellery.JPG"
-                  alt="Presentational Dovile Jewellery piece"
-                  onKeyDown={this.handleKeyDown}
+                  alt="Presentational Dovile Jewellery art piece"
+                  onKeyDown={e =>
+                    this.handleKeyDown(
+                      e,
+                      'FLOW n°B8 and n°B10 | brooches | Baltic amber, ebony, steel'
+                    )
+                  }
                 />
               </Image>
               <Image>
                 <Input
                   type="image"
-                  onClick={this.onImageClick}
+                  onClick={e =>
+                    this.onImageClick(
+                      e,
+                      'FLOW n°B6 | brooch | Baltic amber, ebony, steel'
+                    )
+                  }
                   src="/static/images/Dovile Jewellery-2.JPG"
-                  alt="Presentational Dovile Jewellery piece"
-                  onKeyDown={this.handleKeyDown}
+                  alt="Presentational Dovile Jewellery art piece"
+                  onKeyDown={e =>
+                    this.handleKeyDown(
+                      e,
+                      'FLOW n°B6 | brooch | Baltic amber, ebony, steel'
+                    )
+                  }
                 />
               </Image>
             </TwoImages>
@@ -120,28 +158,49 @@ class LandingPageGallery extends React.Component {
               <Image>
                 <Input
                   type="image"
-                  onClick={this.onImageClick}
+                  onClick={e =>
+                    this.onImageClick(
+                      e,
+                      'FLOW n°R29 | ring | Baltic amber, ebony'
+                    )
+                  }
                   src="/static/images/Dovile Jewellery-3.JPG"
-                  alt="Presentational Dovile Jewellery piece"
-                  onKeyDown={this.handleKeyDown}
+                  alt="Presentational Dovile Jewellery art piece"
+                  onKeyDown={e =>
+                    this.handleKeyDown(
+                      e,
+                      'FLOW n°R29 | ring | Baltic amber, ebony'
+                    )
+                  }
                 />
               </Image>
               <Image>
                 <Input
                   type="image"
-                  onClick={this.onImageClick}
+                  onClick={e =>
+                    this.onImageClick(
+                      e,
+                      'FLOW n°N9 | chest piece | Baltic amber, bog oak, oxidized silver, silk'
+                    )
+                  }
                   src="/static/images/Dovile Jewellery-4.JPG"
-                  alt="Presentational Dovile Jewellery piece"
-                  onKeyDown={this.handleKeyDown}
+                  alt="Presentational Dovile Jewellery art piece"
+                  onKeyDown={e =>
+                    this.handleKeyDown(
+                      e,
+                      'FLOW n°N9 | chest piece | Baltic amber, bog oak, oxidized silver, silk'
+                    )
+                  }
                 />
               </Image>
             </TwoImages>
           </ImagesWrapper>
-          <Headline>
-            <Typography variant="h5" component="h2">
-              contemporary amber art jewellery
-            </Typography>
-          </Headline>
+          <Typography variant="h5" component="h2" className={classes.headline}>
+            contemporary art jewellery
+          </Typography>
+          <Typography variant="h5" component="h2" className={classes.author}>
+            by Dovile Kondrasovaite
+          </Typography>
         </Content>
       </Wrapper>
     );
