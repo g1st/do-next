a8891c2ee2c1bf75c6d50763390f088908e07786
g1st <gintstan@gmail.com>
2019-05-10 15:28:06 +0100

    fix not working private vars (and tehy arent private here anyway)

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  authUrl: process.env.AUTH_URL || 'http://localhost:3030',
-  appUrl: process.env.APP_URL || 'http://localhost:3000',
+  authUrl: 'https://auth-server.gstankus.com',
+  appUrl: 'https://dovile-jewellery.gstankus.com',
   shippingPrice: 5,
   ITEMS_PER_PAGE: 28
 };
