b9999f4edf0ba2613906c152cf03d03449e73cff
g1st <gintstan@gmail.com>
2019-05-10 13:54:41 +0100

    change var name to match one set by dokku

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const { MONGODB_URL } = process.env;
+const { MONGO_URL } = process.env;
 const PORT = process.env.PORT || 3000;
 
 // credits to http://thecodebarbarian.com/building-a-nextjs-app-with-mongodb.html
@@ -36,8 +36,8 @@ co(function*() {
   // Initialize the Next.js app
   yield app.prepare();
 
-  console.log(`Connecting to ${MONGODB_URL}`);
-  mongoose.connect(MONGODB_URL, { useNewUrlParser: true });
+  console.log(`Connecting to ${MONGO_URL}`);
+  mongoose.connect(MONGO_URL, { useNewUrlParser: true });
   const db = yield mongoose.connection;
 
   // Configure express to expose a REST API
