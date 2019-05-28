9b3c470891830ee09e1e3b52a28729c71d35e38d
g1st <gintstan@gmail.com>
2019-05-28 21:26:49 +0100

    add subscribers model

---
+++ api/models/subscribers.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const subscribersSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true
+    },
+    created: { type: Date, default: Date.now }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Subscriber', subscribersSchema);
