554a8213c7c2a02fd98c10e27eb8380bcf1e7d91
g1st <gintstan@gmail.com>
2019-06-05 19:05:52 +0100

    fixl inting

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,7 @@ class MyDocument extends Document {
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
           <script
+            /* eslint-disable-next-line react/no-danger */
             dangerouslySetInnerHTML={{
               __html: `
             window.dataLayer = window.dataLayer || [];
