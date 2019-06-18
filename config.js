1e3f42aca32e21155e0c7a22b0bfbe0d45b40281
g1st <gintstan@gmail.com>
2019-06-18 19:48:23 +0100

    increase items per page number

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   authUrl: 'https://auth-server.gstankus.com',
   appUrl: 'https://dovile-jewellery.gstankus.com',
-  ITEMS_PER_PAGE: 28,
+  ITEMS_PER_PAGE: 30,
   instagramToken: '1759380932.1677ed0.a61e137a154c4c4399b3f1607fe7715a'
 };
