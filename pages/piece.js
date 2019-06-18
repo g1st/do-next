9a1d38b44f5e975e343ee4053c6fecbd2bde6b2f
g1st <gintstan@gmail.com>
2019-06-18 19:38:10 +0100

    refactor to class component

--- pages/piece.js
+++ pages/piece.js
@@ -22,10 +22,13 @@ import {
   Text,
   AnchorLink,
   ListInfo,
-  AdminLink
+  AdminLink,
+  SizesWrapper
 } from '../styles/Piece';
 import { Mail, Strong } from '../styles/Shared';
 import DialogForm from '../components/DialogForm/DialogForm';
+import SizeInput from '../components/Piece/SizeInput';
+import SizesInfo from '../components/Piece/SizesDialog';
 import { pluralise } from '../util/helpers';
 import * as gtag from '../lib/gtag';
 
@@ -51,256 +54,298 @@ const styles = {
   }
 };
 
-const Piece = ({
-  classes,
-  onePieceData,
-  collections,
-  user,
-  addToCart: addToCartRedux,
-  buyItNow: buyItNowRedux,
-  data
-}) => {
-  const handleBuyItNow = item => {
+class Piece extends React.Component {
+  state = {
+    size: '',
+    error: false
+  };
+
+  shouldComponentUpdate(nextProps, nextState) {
+    const { size } = this.state;
+    if (size !== nextState.size) return false;
+    return true;
+  }
+
+  handleBuyItNow = item => {
+    const { buyItNow: buyItNowRedux } = this.props;
+    const { size } = this.state;
     gtag.event({
       action: 'click_buyitnow',
       category: 'Purchase',
       label: 'BuyItNow'
     });
 
+    if (item.category === 'ring' && size === '') {
+      return this.setState({ error: true });
+    }
+    item.ringSize = size;
     buyItNowRedux(item);
     Router.push('/checkout');
   };
 
-  const handleAddToCart = cartData => {
+  handleAddToCart = cartData => {
+    const { addToCart: addToCartRedux } = this.props;
+    const { size } = this.state;
+
     gtag.event({
       action: 'click_addToCart',
       category: 'Purchase',
       label: 'AddToCart'
     });
 
+    if (cartData.category === 'ring' && size === '') {
+      return this.setState({ error: true });
+    }
+    cartData.ringSize = size;
     addToCartRedux(cartData);
   };
 
-  if (onePieceData.length < 1 || onePieceData[0] === null) {
-    return <p>Page doesn't exist</p>;
-  }
-
-  const {
-    name,
-    description,
-    materials,
-    price,
-    size,
-    weight,
-    _id,
-    images,
-    available,
-    group: collection,
-    category,
-    frontImage,
-    madeToOrder,
-    producingTime
-  } = onePieceData[0];
-
-  const dataForCart = {
-    name,
-    price,
-    images,
-    _id,
-    available,
-    quantity: 1,
-    madeToOrder
+  handleSizeChange = ({ target: { value } }) => {
+    this.setState({ size: value, error: false });
   };
 
-  const gallery = images.reduce((acc, image) => {
-    const galleryFormatted = {
-      original: `/static/uploads/${image.medium}`,
-      thumbnail: `/static/uploads/${image.thumb}`,
-      originalAlt: description,
-      thumbnailAlt: name,
-      srcSet: `/static/uploads/${image.medium} 300w, /static/uploads/${image.big} 900w`,
-      sizes: '(max-width: 800px) 80vw, (max-width: 960px) 65vw, 45vw'
-    };
-    if (image.medium === frontImage) {
-      // add in front of array to be first in gallery
-      acc.unshift(galleryFormatted);
-    } else {
-      acc.push(galleryFormatted);
+  render() {
+    const { onePieceData, classes, user, collections, data } = this.props;
+    const { error, size: ringSize } = this.state;
+
+    if (onePieceData.length < 1 || onePieceData[0] === null) {
+      return <p>Page doesn't exist</p>;
     }
-    return acc;
-  }, []);
 
-  const edit = (
-    <div>
-      <Link href={`/edit?id=${_id}`} as={`/edit/${_id}`}>
-        <AdminLink>
-          <Typography inline variant="body1">
-            Edit
-          </Typography>
-        </AdminLink>
-      </Link>
-      <Link href="/admin">
-        <AdminLink>
-          <Typography inline variant="body1">
-            Add new
-          </Typography>
-        </AdminLink>
-      </Link>
-    </div>
-  );
+    const {
+      name,
+      description,
+      materials,
+      price,
+      size,
+      weight,
+      _id,
+      images,
+      available,
+      group: collection,
+      category,
+      frontImage,
+      madeToOrder,
+      producingTime
+    } = onePieceData[0];
 
-  const notAvailable = (
-    <Typography variant="body2" paragraph>
-      This item is <Strong>NOT AVAILABLE</Strong> for purchasing because of
-      exhibiting at the various events or it’s already sold.
-    </Typography>
-  );
+    const dataForCart = {
+      name,
+      price,
+      images,
+      _id,
+      available,
+      quantity: 1,
+      madeToOrder,
+      category,
+      ringSize
+    };
 
-  const pathLine = (
-    <div>
-      <Link href="/gallery">
-        <AnchorLink>
-          <Typography inline variant="body2" className={classes.filterLine}>
-            gallery
-          </Typography>
-        </AnchorLink>
-      </Link>
-      <ArrowRight fontSize="small" className={classes.svg} />
-      <Link
-        href={`/gallery?collection=${collection}`}
-        as={`/gallery/${collection}`}
-      >
-        <AnchorLink>
-          <Typography inline variant="body2" className={classes.filterLine}>
-            {collection}
-          </Typography>
-        </AnchorLink>
-      </Link>
-      <ArrowRight fontSize="small" className={classes.svg} />
-      <Typography inline variant="body2" className={classes.filterLine}>
-        {pluralise(category)}
+    const gallery = images.reduce((acc, image) => {
+      const galleryFormatted = {
+        original: `/static/uploads/${image.medium}`,
+        thumbnail: `/static/uploads/${image.thumb}`,
+        originalAlt: description,
+        thumbnailAlt: name,
+        srcSet: `/static/uploads/${image.medium} 300w, /static/uploads/${image.big} 900w`,
+        sizes: '(max-width: 800px) 80vw, (max-width: 960px) 65vw, 45vw'
+      };
+      if (image.medium === frontImage) {
+        // add in front of array to be first in gallery
+        acc.unshift(galleryFormatted);
+      } else {
+        acc.push(galleryFormatted);
+      }
+      return acc;
+    }, []);
+
+    const edit = (
+      <div>
+        <Link href={`/edit?id=${_id}`} as={`/edit/${_id}`}>
+          <AdminLink>
+            <Typography inline variant="body1">
+              Edit
+            </Typography>
+          </AdminLink>
+        </Link>
+        <Link href="/admin">
+          <AdminLink>
+            <Typography inline variant="body1">
+              Add new
+            </Typography>
+          </AdminLink>
+        </Link>
+      </div>
+    );
+
+    const notAvailable = (
+      <Typography variant="body2" paragraph>
+        This item is <Strong>NOT AVAILABLE</Strong> for purchasing because of
+        exhibiting at the various events or it’s already sold.
       </Typography>
-    </div>
-  );
+    );
 
-  return (
-    <Layout
-      pathname="/gallery"
-      collections={collections}
-      title={`${name} | Dovile Jewellery`}
-      description={`Materials: ${materials}
+    const pathLine = (
+      <div>
+        <Link href="/gallery">
+          <AnchorLink>
+            <Typography inline variant="body2" className={classes.filterLine}>
+              gallery
+            </Typography>
+          </AnchorLink>
+        </Link>
+        <ArrowRight fontSize="small" className={classes.svg} />
+        <Link
+          href={`/gallery?collection=${collection}`}
+          as={`/gallery/${collection}`}
+        >
+          <AnchorLink>
+            <Typography inline variant="body2" className={classes.filterLine}>
+              {collection}
+            </Typography>
+          </AnchorLink>
+        </Link>
+        <ArrowRight fontSize="small" className={classes.svg} />
+        <Typography inline variant="body2" className={classes.filterLine}>
+          {pluralise(category)}
+        </Typography>
+      </div>
+    );
+
+    return (
+      <Layout
+        pathname="/gallery"
+        collections={collections}
+        title={`${name} | Dovile Jewellery`}
+        description={`Materials: ${materials}
       ${description}`}
-    >
-      {pathLine}
-      <Wrapper>
-        <Images>
-          <ImageGallery
-            items={gallery}
-            lazyLoad
-            showNav
-            showPlayButton={false}
-            showFullscreenButton
-            renderItem={customItem}
-            renderThumbInner={customThumb}
-          />
-        </Images>
-        <Info>
-          <Text>
-            {user && edit}
-            <Typography variant="h4">{name}</Typography>
-            <Typography variant="h5" classes={{ h5: classes.marginBottomBig }}>
-              £{price}
-            </Typography>
-            {description.split('\n').map((paragraph, i) => (
-              <Typography key={i} variant="body2">
-                {paragraph}
-              </Typography>
-            ))}
-            <ListInfo>
-              {materials && (
-                <li>
-                  <Typography variant="body2">
-                    Materials: {materials}
-                  </Typography>
-                </li>
-              )}
-              {size && (
-                <li>
-                  <Typography variant="body2">Dimensions: {size}</Typography>
-                </li>
-              )}
-              {weight && (
-                <li>
-                  <Typography variant="body2">Weight: {weight}</Typography>
-                </li>
-              )}
-              {available && madeToOrder && (
-                <li>
-                  <Typography variant="body2">
-                    This is <Strong>made to order</Strong> item. PLEASE NOTE
-                    that estimated producing time for this item is{' '}
-                    {producingTime}.
-                  </Typography>
-                </li>
-              )}
-            </ListInfo>
-
-            <Typography variant="body2" paragraph>
-              All purchases come in a branded box.
-            </Typography>
-            <Typography variant="body2" paragraph>
-              All items being sold as gold or silver are hallmarked to confirm
-              that they meet the legal standard in accordance with the British
-              Assay Office.
-            </Typography>
-
-            {available ? (
-              <ButtonsWrapper>
-                <Button
-                  size="medium"
-                  variant="contained"
-                  color="primary"
-                  className={classes.button}
-                  onClick={() => handleBuyItNow(dataForCart)}
-                >
-                  Buy It Now
-                </Button>
-                <Button
-                  size="medium"
-                  variant="contained"
-                  color="secondary"
-                  onClick={() => handleAddToCart(dataForCart)}
-                  className={classes.button}
-                >
-                  Add To Cart
-                </Button>
-              </ButtonsWrapper>
-            ) : (
-              notAvailable
-            )}
-            <Typography variant="body2" paragraph>
-              Please note most of the pieces are ONE OF A KIND, therefore,
-              commissions are welcome in related designs to meet your personal
-              needs.
-            </Typography>
-            <Typography variant="body2" paragraph>
-              If interested in ordering customized design please contact me
-              directly to{' '}
-              <Mail
-                href="mailto:&#104;&#101;&#108;&#108;&#111;&#064;&#100;&#111;&#118;&#105;&#108;&#101;&#106;&#101;&#119;&#101;&#108;&#108;&#101;&#114;&#121;&#046;&#099;&#111;&#109;"
-                target="_top"
+      >
+        {pathLine}
+        <Wrapper>
+          <Images>
+            <ImageGallery
+              items={gallery}
+              lazyLoad
+              showNav
+              showPlayButton={false}
+              showFullscreenButton
+              renderItem={customItem}
+              renderThumbInner={customThumb}
+            />
+          </Images>
+          <Info>
+            <Text>
+              {user && edit}
+              <Typography variant="h4">{name}</Typography>
+              <Typography
+                variant="h5"
+                classes={{ h5: classes.marginBottomBig }}
               >
-                &#104;&#101;&#108;&#108;&#111;&#064;&#100;&#111;&#118;&#105;&#108;&#101;&#106;&#101;&#119;&#101;&#108;&#108;&#101;&#114;&#121;&#046;&#099;&#111;&#109;
-              </Mail>
-            </Typography>
-            {available ? null : <DialogForm />}
-          </Text>
-        </Info>
-      </Wrapper>
-      <PieceGallery data={data} currentItem={onePieceData[0]} />
-    </Layout>
-  );
-};
+                £{price}
+              </Typography>
+              {description.split('\n').map((paragraph, i) => (
+                <Typography key={i} variant="body2">
+                  {paragraph}
+                </Typography>
+              ))}
+              <ListInfo>
+                {materials && (
+                  <li>
+                    <Typography variant="body2">
+                      Materials: {materials}
+                    </Typography>
+                  </li>
+                )}
+                {size && (
+                  <li>
+                    <Typography variant="body2">Dimensions: {size}</Typography>
+                  </li>
+                )}
+                {weight && (
+                  <li>
+                    <Typography variant="body2">Weight: {weight}</Typography>
+                  </li>
+                )}
+                {available && madeToOrder && (
+                  <li>
+                    <Typography variant="body2">
+                      This is <Strong>made to order</Strong> item. PLEASE NOTE
+                      that estimated producing time for this item is{' '}
+                      {producingTime}.
+                    </Typography>
+                  </li>
+                )}
+              </ListInfo>
+
+              <Typography variant="body2" paragraph>
+                All purchases come in a branded box.
+              </Typography>
+              <Typography variant="body2" paragraph>
+                All items being sold as gold or silver are hallmarked to confirm
+                that they meet the legal standard in accordance with the British
+                Assay Office.
+              </Typography>
+
+              {available ? (
+                <div>
+                  {category === 'ring' ? (
+                    <SizesWrapper>
+                      <SizeInput
+                        handleChange={this.handleSizeChange}
+                        error={error}
+                        errorText="Please select ring size."
+                      />
+                      <SizesInfo />
+                    </SizesWrapper>
+                  ) : null}
+                  <ButtonsWrapper>
+                    <Button
+                      size="medium"
+                      variant="contained"
+                      color="primary"
+                      className={classes.button}
+                      onClick={() => this.handleBuyItNow(dataForCart)}
+                    >
+                      Buy It Now
+                    </Button>
+                    <Button
+                      size="medium"
+                      variant="contained"
+                      color="secondary"
+                      onClick={() => this.handleAddToCart(dataForCart)}
+                      className={classes.button}
+                    >
+                      Add To Cart
+                    </Button>
+                  </ButtonsWrapper>
+                </div>
+              ) : (
+                notAvailable
+              )}
+              <Typography variant="body2" paragraph>
+                Please note most of the pieces are ONE OF A KIND, therefore,
+                commissions are welcome in related designs to meet your personal
+                needs.
+              </Typography>
+              <Typography variant="body2" paragraph>
+                If interested in ordering customized design please contact me
+                directly to{' '}
+                <Mail
+                  href="mailto:&#104;&#101;&#108;&#108;&#111;&#064;&#100;&#111;&#118;&#105;&#108;&#101;&#106;&#101;&#119;&#101;&#108;&#108;&#101;&#114;&#121;&#046;&#099;&#111;&#109;"
+                  target="_top"
+                >
+                  &#104;&#101;&#108;&#108;&#111;&#064;&#100;&#111;&#118;&#105;&#108;&#101;&#106;&#101;&#119;&#101;&#108;&#108;&#101;&#114;&#121;&#046;&#099;&#111;&#109;
+                </Mail>
+              </Typography>
+              <DialogForm />
+            </Text>
+          </Info>
+        </Wrapper>
+        <PieceGallery data={data} currentItem={onePieceData[0]} />
+      </Layout>
+    );
+  }
+}
 
 Piece.propTypes = {
   classes: PropTypes.object.isRequired,
