80184aedd53a7765329caa7fc7e7fc616536fb06
g1st <gintstan@gmail.com>
2019-06-21 08:21:24 +0100

    update url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  authUrl: 'https://auth-server.gstankus.com',
-  appUrl: 'https://dovile-jewellery.gstankus.com',
+  authUrl: 'https://auth-server.dovilejewellery.com',
+  appUrl: 'https://dovilejewellery.com',
   ITEMS_PER_PAGE: 30,
   instagramToken: '1759380932.1677ed0.a61e137a154c4c4399b3f1607fe7715a'
 };
