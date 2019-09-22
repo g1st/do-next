9ed9fe1051bfc9b7e60faaf2862b74c192affb08
g1st <gintstan@gmail.com>
2019-09-22 14:48:06 +0100

    Update styles for JOYA banner.

--- styles/UpcomingEvent.js
+++ styles/UpcomingEvent.js
@@ -1,89 +1,10 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div`
-  max-width: 100%;
-  min-height: 186px;
-  margin: 20px auto 60px;
-  display: flex;
-  align-items: center;
-  background-image: url('/static/images/bg1.JPG');
-  background-color: #eeeeee;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-
-  @media (min-width: 700px) {
-    margin: 40px auto 80px;
-  }
+export const EventWrapper = styled.div`
+  margin-bottom: 4em;
 `;
-Wrapper.displayName = 'Wrapper';
 
-export const Data = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-Data.displayName = 'Data';
-
-export const Content = styled.div`
+export const Image = styled.img`
+  height: 100%;
   width: 100%;
-  flex-direction: column;
-  display: flex;
-  justify-content: center;
-  user-select: none;
-
-  @media (min-width: 700px) {
-    margin: 30px auto 0 auto;
-    align-items: center;
-  }
-  @media (min-width: 800px) {
-    margin-top: 26px;
-  }
-  @media (min-width: 900px) {
-    margin-top: 0;
-  }
 `;
-Content.displayName = 'Content';
-
-export const FlexItem = styled.div`
-  @media (min-width: 700px) {
-    display: flex;
-  }
-`;
-FlexItem.displayName = 'FlexItem';
-
-export const AnchorLink = styled.a`
-  text-decoration: none;
-`;
-AnchorLink.displayName = 'AnchorLink';
-
-export const WrapData = styled.div`
-  @media (min-width: 700px) {
-    display: flex;
-  }
-`;
-WrapData.displayName = 'WrapData';
-
-export const DataInner = styled.div`
-  @media (min-width: 700px) {
-    margin-top: 4px;
-    display: flex;
-    flex-direction: column-reverse;
-    justify-content: space-between;
-    height: 70px;
-  }
-  @media (min-width: 800px) {
-    margin-top: 6px;
-    height: 80px;
-  }
-`;
-DataInner.displayName = 'DataInner';
-
-export const ForBiggerScreens = styled.div`
-  display: none;
-  @media (min-width: 900px) {
-    display: flex;
-    align-items: center;
-    margin: 0 auto;
-  }
-`;
-ForBiggerScreens.displayName = 'ForBiggerScreens';
