1eae39ec400f4950e63d853b513eacc6b6199de0
g1st <gintstan@gmail.com>
2019-10-29 20:01:22 +0000

    change  image

--- components/UpcomingEvent/UpcomingEvent.js
+++ components/UpcomingEvent/UpcomingEvent.js
@@ -6,7 +6,7 @@ const UpcomingEvent = () => (
   <EventWrapper>
     <Link href="/gallery?collection=silver flow" as="/gallery/silver flow">
       <Image
-        src="../../static/images/Silver-FLOW-banner.jpeg"
+        src="../../static/images/Banner-1.JPG"
         alt="Silver Flow collection banner"
         sizes=""
         srcSet=""
