4f9abb2e3b35de962c84dd3e4c7172e72ef2117a
g1st <gintstan@gmail.com>
2019-10-30 20:45:37 +0000

    update link

--- components/UpcomingEvent/UpcomingEvent.js
+++ components/UpcomingEvent/UpcomingEvent.js
@@ -4,7 +4,7 @@ import { Image, EventWrapper } from '../../styles/UpcomingEvent';
 
 const UpcomingEvent = () => (
   <EventWrapper>
-    <Link href="/gallery?collection=silver flow" as="/gallery/silver flow">
+    <Link href="/gallery?collection=silver-flow" as="/gallery/silver-flow">
       <Image
         src="../../static/images/Banner-1.JPG"
         alt="Silver Flow collection banner"
