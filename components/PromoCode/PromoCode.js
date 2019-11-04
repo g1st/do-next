58849ab5abf6577ca08248245875c99636fe3cfc
g1st <gintstan@gmail.com>
2019-11-04 21:18:56 +0000

    updating UI [wip]

--- components/PromoCode/PromoCode.js
+++ components/PromoCode/PromoCode.js
@@ -15,6 +15,7 @@ import ClearIcon from '@material-ui/icons/Clear';
 import CheckIcon from '@material-ui/icons/Check';
 import { withStyles } from '@material-ui/core/styles';
 
+import Error from '../Error/Error';
 import { appUrl } from '../../config';
 import { addDiscount } from '../../store/actions';
 
@@ -74,10 +75,10 @@ const styles = {
 };
 
 const PromoCode = ({ classes }) => {
-  const [input, setInput] = useState(false);
+  const [inputVisible, setInputVisible] = useState(false);
   const [code, setCode] = useState('');
   const [submit, setSubmit] = useState('');
-  const [data, setData] = useState('');
+  const [promoCodeApplied, setPromoCodeApplied] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
   const firstLoad = useRef();
@@ -98,10 +99,12 @@ const PromoCode = ({ classes }) => {
         const { validCode } = result.data;
         console.log(validCode);
         setIsLoading(false);
-        setData(validCode);
+        setPromoCodeApplied(validCode);
         if (validCode) {
           console.log('dispatchina');
           dispatch(addDiscount(submit));
+        } else {
+          setError(true);
         }
       } catch (err) {
         setError(err);
@@ -112,7 +115,7 @@ const PromoCode = ({ classes }) => {
     if (firstLoad.current) {
       checkPromoCode();
     }
-  }, [dispatch, firstLoad, setData, submit]);
+  }, [dispatch, firstLoad, submit]);
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -120,74 +123,95 @@ const PromoCode = ({ classes }) => {
     console.log('submit');
   };
 
+  const handleKeyDown = ({ key }) => {
+    if (key === 'Enter' || key === ' ') {
+      setInputVisible(!inputVisible);
+    }
+  };
+
   return (
     <ListItem>
       <ListItemText className={classes.textRight}>
-        {!input && (
-          <button type="button" className={classes.button}>
-            <Typography variant="body2" onClick={() => setInput(!input)}>
-              Got promo code?
-            </Typography>
-          </button>
-        )}
-        {input &&
-          (data ? (
-            <div>promo successfully applied</div>
-          ) : (
-            <form onSubmit={handleSubmit}>
-              <IconButton
-                style={{ color: 'rgba(0, 0, 0, 0.26)', marginLeft: '4px' }}
-                aria-label="Toggle promo code input"
-                onClick={() => {
-                  setInput(!input);
-                  setCode('');
-                }}
-              >
-                <ClearIcon fontSize="small" />
-              </IconButton>
-              <Input
-                ref={firstLoad}
-                value={code}
-                id="promocode"
-                inputProps={{
-                  'aria-label': 'Promotional code',
-                  required: true
-                }}
-                type="text"
-                disableUnderline
-                className={classes.inputRoot}
-                onChange={e => setCode(e.target.value)}
-                error={error}
-                placeholder="Promo code"
-                disabled={isLoading}
-              />
-              <div className={classes.root}>
-                <div className="wrapper">
-                  {data ? (
-                    <CheckIcon classes={{ root: classes.checkIcon }} />
-                  ) : (
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      type="submit"
-                      size="small"
-                      className={data ? classes.buttonSuccess : ''}
-                      disabled={isLoading}
-                    >
-                      Apply
-                      {isLoading && (
-                        <CircularProgress
-                          size={24}
-                          className={classes.buttonProgress}
-                        />
+        {discount ? (
+          <Typography variant="body2">-{discount}% off applied</Typography>
+        ) : (
+          <>
+            {!inputVisible && (
+              <button type="button" className={classes.button}>
+                <Typography
+                  variant="body2"
+                  onClick={() => setInputVisible(!inputVisible)}
+                  onKeyDown={handleKeyDown}
+                  tabIndex={0}
+                  role="button"
+                >
+                  Got promo code?
+                </Typography>
+              </button>
+            )}
+            {inputVisible &&
+              (promoCodeApplied ? null : (
+                <form onSubmit={handleSubmit}>
+                  <IconButton
+                    style={{
+                      color: 'rgba(0, 0, 0, 0.26)',
+                      marginLeft: '4px'
+                    }}
+                    aria-label="Toggle promo code input"
+                    onClick={() => {
+                      setInputVisible(!inputVisible);
+                      setCode('');
+                    }}
+                  >
+                    <ClearIcon fontSize="small" />
+                  </IconButton>
+                  <Input
+                    ref={firstLoad}
+                    value={code}
+                    id="promocode"
+                    inputProps={{
+                      'aria-label': 'Promotional code',
+                      required: true
+                    }}
+                    type="text"
+                    disableUnderline
+                    className={classes.inputRoot}
+                    onChange={e => setCode(e.target.value)}
+                    error={error}
+                    placeholder="Promo code"
+                    disabled={isLoading}
+                  />
+                  <div className={classes.root}>
+                    <div className="wrapper">
+                      {promoCodeApplied ? (
+                        <CheckIcon classes={{ root: classes.checkIcon }} />
+                      ) : (
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          type="submit"
+                          size="small"
+                          className={
+                            promoCodeApplied ? classes.buttonSuccess : ''
+                          }
+                          disabled={isLoading}
+                        >
+                          Apply
+                          {isLoading && (
+                            <CircularProgress
+                              size={24}
+                              className={classes.buttonProgress}
+                            />
+                          )}
+                        </Button>
                       )}
-                    </Button>
-                  )}
-                </div>
-              </div>
-            </form>
-          ))}
-        <div>discount: {discount}</div>
+                    </div>
+                  </div>
+                  {error && <Error>Wrong code</Error>}
+                </form>
+              ))}
+          </>
+        )}
       </ListItemText>
     </ListItem>
   );
