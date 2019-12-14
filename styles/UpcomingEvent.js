6ec02ad56b213b76e099e9a302aa656242a4303d
g1st <gintstan@gmail.com>
2019-12-14 17:16:54 +0000

    increase margin

--- styles/UpcomingEvent.js
+++ styles/UpcomingEvent.js
@@ -1,11 +1,10 @@
 import styled from 'styled-components';
 
 export const EventWrapper = styled.div`
-  margin-bottom: 4em;
+  margin-bottom: 6em;
 `;
 
 export const Image = styled.img`
-  cursor: ${({ cursorDefault }) => (cursorDefault ? 'default' : 'pointer')};
   height: 100%;
   width: 100%;
 `;
