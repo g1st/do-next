009b30e4d7c03d1cd951a842d46d99a1d683abde
g1st <gintstan@gmail.com>
2019-12-14 16:47:29 +0000

    update mock data

--- util/seed/data.js
+++ util/seed/data.js
@@ -2,256 +2,120 @@ module.exports = [
   {
     images: [
       {
-        big: 'a85f1a24-4404-4b58-9a23-8c2868f97da7.jpg',
-        medium: 'a85f1a24-4404-4b58-9a23-8c2868f97da7300.jpg',
-        thumb: 'a85f1a24-4404-4b58-9a23-8c2868f97da792.jpg'
+        big: '73ffa692-c89a-4bd2-92cf-7fbbc69f849d.JPG',
+        medium: '73ffa692-c89a-4bd2-92cf-7fbbc69f849d300.JPG',
+        thumb: '73ffa692-c89a-4bd2-92cf-7fbbc69f849d92.JPG'
       },
       {
-        big: '02a909ee-b914-4d51-9a88-5c567444dd8d.jpg',
-        medium: '02a909ee-b914-4d51-9a88-5c567444dd8d300.jpg',
-        thumb: '02a909ee-b914-4d51-9a88-5c567444dd8d92.jpg'
+        big: '344d58bc-fb26-4938-9b2a-025c6b627491.JPG',
+        medium: '344d58bc-fb26-4938-9b2a-025c6b627491300.JPG',
+        thumb: '344d58bc-fb26-4938-9b2a-025c6b62749192.JPG'
       },
       {
-        big: 'e9af49bf-a942-44d1-a88a-a0df04d9380b.jpg',
-        medium: 'e9af49bf-a942-44d1-a88a-a0df04d9380b300.jpg',
-        thumb: 'e9af49bf-a942-44d1-a88a-a0df04d9380b92.jpg'
+        big: 'ca89b30d-62fb-4fed-9c24-1ad6dda8a425.JPG',
+        medium: 'ca89b30d-62fb-4fed-9c24-1ad6dda8a425300.JPG',
+        thumb: 'ca89b30d-62fb-4fed-9c24-1ad6dda8a42592.JPG'
       },
       {
-        big: '702cb7a9-896d-4880-a265-0c873f55227d.jpg',
-        medium: '702cb7a9-896d-4880-a265-0c873f55227d300.jpg',
-        thumb: '702cb7a9-896d-4880-a265-0c873f55227d92.jpg'
-      },
-      {
-        big: 'ad0736e5-12e1-46d1-b146-61a689aa985b.jpg',
-        medium: 'ad0736e5-12e1-46d1-b146-61a689aa985b300.jpg',
-        thumb: 'ad0736e5-12e1-46d1-b146-61a689aa985b92.jpg'
+        big: '51385e27-534f-4b12-a99a-3d30f55565ae.JPG',
+        medium: '51385e27-534f-4b12-a99a-3d30f55565ae300.JPG',
+        thumb: '51385e27-534f-4b12-a99a-3d30f55565ae92.JPG'
       }
     ],
-    materials: ['Sterling silver, 18 ct yellow gold, tanzanite.'],
     category: 'ring',
     available: true,
-    name: "n'8",
-    slug: "n'8",
-    description:
-      'Blue Variations is the collection of statement rings which is still growing. My attraction for organic shapes and colours transferred into a form of independent statement ring. All shapes are designed to fit very well on the middle finger overcrossing other fingers. I wanted to create my signature ring which is sculptural, bold and at the same time quite delicate with the finish of gemstones, oxides, platings.',
-    group: 'blue variations',
-    size: 'Ring size M, (inside diameter) 16.5mm.',
-    price: 180
-  },
-  {
-    images: [
-      {
-        big: 'e1834dc6-4bee-460d-b189-07984ba46266.jpg',
-        medium: 'e1834dc6-4bee-460d-b189-07984ba46266300.jpg',
-        thumb: 'e1834dc6-4bee-460d-b189-07984ba4626692.jpg'
-      },
-      {
-        big: '8091d5f2-6f48-4cca-abd1-81cbcd65083f.jpg',
-        medium: '8091d5f2-6f48-4cca-abd1-81cbcd65083f300.jpg',
-        thumb: '8091d5f2-6f48-4cca-abd1-81cbcd65083f92.jpg'
-      },
-      {
-        big: '49bfc75c-eac2-4bdb-9f36-755a3a3d30c7.webp',
-        medium: '49bfc75c-eac2-4bdb-9f36-755a3a3d30c7300.webp',
-        thumb: '49bfc75c-eac2-4bdb-9f36-755a3a3d30c792.webp'
-      }
-    ],
-    materials: ['Sterling silver, sapphires.'],
-    category: 'ring',
-    available: true,
-    name: "n'23",
-    slug: "n'23",
-    description:
-      'Blue Variations is the collection of statement rings which is still growing. My attraction for organic shapes and colours transferred into a form of independent statement ring. All shapes are designed to fit very well on the middle finger overcrossing other fingers. I wanted to create my signature ring which is sculptural, bold and at the same time quite delicate with the finish of gemstones, oxides, platings.',
-    group: 'blue variations',
-    size: 'Ring size O (inside diameter) 17.3mm, Top surface length 35mm.',
-    price: 185
-  },
-  {
-    images: [
-      {
-        big: '4e26d76c-a6d7-4e57-9ba7-be46d945da7d.jpg',
-        medium: '4e26d76c-a6d7-4e57-9ba7-be46d945da7d300.jpg',
-        thumb: '4e26d76c-a6d7-4e57-9ba7-be46d945da7d92.jpg'
-      },
-      {
-        big: 'eb33159b-3765-4443-a8da-c89610a9cff2.jpg',
-        medium: 'eb33159b-3765-4443-a8da-c89610a9cff2300.jpg',
-        thumb: 'eb33159b-3765-4443-a8da-c89610a9cff292.jpg'
-      },
-      {
-        big: '031e7f1c-c1fe-4ed2-9c82-d8322f527706.jpg',
-        medium: '031e7f1c-c1fe-4ed2-9c82-d8322f527706300.jpg',
-        thumb: '031e7f1c-c1fe-4ed2-9c82-d8322f52770692.jpg'
-      },
-      {
-        big: '258aca8b-e9fc-47ed-80a4-2d920624f28d.jpg',
-        medium: '258aca8b-e9fc-47ed-80a4-2d920624f28d300.jpg',
-        thumb: '258aca8b-e9fc-47ed-80a4-2d920624f28d92.jpg'
-      }
-    ],
-    materials: ['Ebony, oxidized silver, Baltic amber'],
-    category: 'brooch',
-    available: true,
-    name: "n'B1",
-    slug: "n'B1",
-    description:
-      'Hand-carved ebony brooch with a detail of Baltic amber and oxidized silver back.\n\nThe central philosophical figure in the concept is TIME which reflects through the materials used, their interplay giving a sense of the growth to the wearer.\n\nMy task was to make an object which itself cannot be measured clearly within a time scale. The shapes I am making could look like driftwood or a growing branch. To me ageing is an absolutely gorgeous process which naturally creates such great lines no matter if it is my face getting wrinkles, a tree bark or a bud of flower starting to bloom or even a coming wave. It could take seconds or centuries.',
-    group: 'flow',
+    madeToOrder: true,
+    producingTime: '1 week',
+    display: true,
+    oneOfAKind: false,
+    name: '1',
+    slug: '1',
+    description: '1',
+    materials: '',
+    group: 'nauja',
     size: '',
-    price: 285
+    weight: '',
+    price: 1,
+    frontImage: '73ffa692-c89a-4bd2-92cf-7fbbc69f849d300.JPG',
+    galleryIndex: 56,
+    collectionIndex: 56
   },
   {
     images: [
       {
-        big: '2441d33e-2b0d-4c61-86e3-f3a8fc28f793.jpg',
-        medium: '2441d33e-2b0d-4c61-86e3-f3a8fc28f793300.jpg',
-        thumb: '2441d33e-2b0d-4c61-86e3-f3a8fc28f79392.jpg'
-      },
-      {
-        big: '0f707b15-2bcf-4b96-bd09-ec0a220e1b10.jpg',
-        medium: '0f707b15-2bcf-4b96-bd09-ec0a220e1b10300.jpg',
-        thumb: '0f707b15-2bcf-4b96-bd09-ec0a220e1b1092.jpg'
-      },
-      {
-        big: '4a75acbb-094e-49f7-837d-dd4a9efc1b30.jpg',
-        medium: '4a75acbb-094e-49f7-837d-dd4a9efc1b30300.jpg',
-        thumb: '4a75acbb-094e-49f7-837d-dd4a9efc1b3092.jpg'
+        big: 'a5ffef80-e965-49f7-8c56-07290ec75a1a.JPG',
+        medium: 'a5ffef80-e965-49f7-8c56-07290ec75a1a300.JPG',
+        thumb: 'a5ffef80-e965-49f7-8c56-07290ec75a1a92.JPG'
       }
     ],
-    materials: [
-      'asymmetric hand-carved ebony, oxidized silver, natural Baltic amber.'
+    category: 'ring',
+    available: true,
+    madeToOrder: true,
+    producingTime: '2 weeks',
+    display: true,
+    oneOfAKind: false,
+    name: 'tst',
+    slug: 'tst',
+    description: 'dsv',
+    materials: '',
+    group: 'silver-flow',
+    size: '',
+    weight: '',
+    price: 123,
+    frontImage: 'a5ffef80-e965-49f7-8c56-07290ec75a1a300.JPG',
+    galleryIndex: 58,
+    collectionIndex: 58
+  },
+  {
+    images: [
+      {
+        big: 'e8d13afc-612f-45b1-bd6b-1f93502cbf70.JPG',
+        medium: 'e8d13afc-612f-45b1-bd6b-1f93502cbf70300.JPG',
+        thumb: 'e8d13afc-612f-45b1-bd6b-1f93502cbf7092.JPG'
+      }
+    ],
+    category: 'ring',
+    available: false,
+    madeToOrder: false,
+    producingTime: '',
+    display: true,
+    oneOfAKind: false,
+    name: 'qqqa',
+    slug: 'qqqa',
+    description: '123',
+    materials: '',
+    group: '123',
+    size: '',
+    weight: '',
+    price: 20,
+    frontImage: 'e8d13afc-612f-45b1-bd6b-1f93502cbf70300.JPG',
+    galleryIndex: 63,
+    collectionIndex: 63
+  },
+  {
+    images: [
+      {
+        big: 'e96dddc9-af28-479f-a067-996d83669e80.JPG',
+        medium: 'e96dddc9-af28-479f-a067-996d83669e80300.JPG',
+        thumb: 'e96dddc9-af28-479f-a067-996d83669e8092.JPG'
+      }
     ],
     category: 'earring',
     available: true,
-    name: "n'E14",
-    slug: "n'E14",
-    description:
-      "Can't leave empty as description is required! :) Need to change that rule.",
-    group: 'flow',
-    size: '90x18mm, each weight ~6.2g.',
-    price: 220
-  },
-  {
-    images: [
-      {
-        big: 'b98abbdb-9b18-4cf7-b9db-a899cbd41b8e.jpg',
-        medium: 'b98abbdb-9b18-4cf7-b9db-a899cbd41b8e300.jpg',
-        thumb: 'b98abbdb-9b18-4cf7-b9db-a899cbd41b8e92.jpg'
-      },
-      {
-        big: '4f9e8a54-a74b-4128-b783-62ef30899e44.jpg',
-        medium: '4f9e8a54-a74b-4128-b783-62ef30899e44300.jpg',
-        thumb: '4f9e8a54-a74b-4128-b783-62ef30899e4492.jpg'
-      },
-      {
-        big: 'c96f5e1b-4b9e-4120-8ef0-9612d587dd6f.jpg',
-        medium: 'c96f5e1b-4b9e-4120-8ef0-9612d587dd6f300.jpg',
-        thumb: 'c96f5e1b-4b9e-4120-8ef0-9612d587dd6f92.jpg'
-      },
-      {
-        big: 'cc77f216-b80e-411e-9375-3a09bd6f2ff0.jpg',
-        medium: 'cc77f216-b80e-411e-9375-3a09bd6f2ff0300.jpg',
-        thumb: 'cc77f216-b80e-411e-9375-3a09bd6f2ff092.jpg'
-      }
-    ],
-    materials: ['hand-carved ebony, oxidized silver, sky blue topaz'],
-    category: 'earring',
-    available: true,
-    name: "n'E22",
-    slug: "n'E22",
-    description:
-      "Can't leave empty as description is required! :) Need to change that rule.",
-    group: 'flow',
-    size: '~35x16mm, only ~3.2g each earring',
-    price: 220
-  },
-  {
-    images: [
-      {
-        big: '91d3183c-468a-4946-a02d-9af03ab4a7db.jpg',
-        medium: '91d3183c-468a-4946-a02d-9af03ab4a7db300.jpg',
-        thumb: '91d3183c-468a-4946-a02d-9af03ab4a7db92.jpg'
-      },
-      {
-        big: 'faafc7cc-519a-4196-997d-c6826450075d.jpg',
-        medium: 'faafc7cc-519a-4196-997d-c6826450075d300.jpg',
-        thumb: 'faafc7cc-519a-4196-997d-c6826450075d92.jpg'
-      },
-      {
-        big: 'c84f484c-6487-4bd3-8bcd-301ed9da040c.jpg',
-        medium: 'c84f484c-6487-4bd3-8bcd-301ed9da040c300.jpg',
-        thumb: 'c84f484c-6487-4bd3-8bcd-301ed9da040c92.jpg'
-      },
-      {
-        big: '2ed1e280-2819-42a2-bad3-48c2c6f76b8a.jpg',
-        medium: '2ed1e280-2819-42a2-bad3-48c2c6f76b8a300.jpg',
-        thumb: '2ed1e280-2819-42a2-bad3-48c2c6f76b8a92.jpg'
-      },
-      {
-        big: 'bd684105-07f0-418c-9dcf-b33dcf74edc7.jpg',
-        medium: 'bd684105-07f0-418c-9dcf-b33dcf74edc7300.jpg',
-        thumb: 'bd684105-07f0-418c-9dcf-b33dcf74edc792.jpg'
-      }
-    ],
-    materials: ['Gold-plated silver, rubies.'],
-    category: 'other',
-    available: true,
-    name: 'HEART LOCKET',
-    slug: 'HEART-LOCKET',
-    description:
-      'For a long time I was planning to make something timeless, inspired by antique jewellery. These lockets could be ideal newborn girl gift. Message me direct for any interest, customised commissions are welcome.',
-    group: 'various',
-    size: '37x17x9mm',
-    price: 335
-  },
-  {
-    images: [
-      {
-        big: '180dd62f-8668-4a07-aca1-4913772e0337.jpg',
-        medium: '180dd62f-8668-4a07-aca1-4913772e0337300.jpg',
-        thumb: '180dd62f-8668-4a07-aca1-4913772e033792.jpg'
-      },
-      {
-        big: 'd794b7a9-817b-4033-ac3b-7007e159fae5.jpg',
-        medium: 'd794b7a9-817b-4033-ac3b-7007e159fae5300.jpg',
-        thumb: 'd794b7a9-817b-4033-ac3b-7007e159fae592.jpg'
-      },
-      {
-        big: 'ca7ca3b1-2dc4-43c3-9b30-7d55a25d1f15.jpg',
-        medium: 'ca7ca3b1-2dc4-43c3-9b30-7d55a25d1f15300.jpg',
-        thumb: 'ca7ca3b1-2dc4-43c3-9b30-7d55a25d1f1592.jpg'
-      }
-    ],
-    materials: ['oxidized silver, Baltic amber.'],
-    category: 'necklace',
-    available: true,
-    name: "n'N1",
-    slug: "n'N1",
-    description:
-      'The central philosophical figure in the concept is TIME which reflects through the materials used, their interplay giving a sense of the growth to the wearer.\n\nMy task was to make an object which itself cannot be measured clearly within a time scale. The shapes I am making could look like driftwood or a growing branch. To me ageing is an absolutely gorgeous process which naturally creates such great lines no matter if it is my face getting wrinkles, a tree bark or a bud of flower starting to bloom or even a coming wave. It could take seconds or centuries.',
-    group: 'flow',
-    size:
-      'ebony piece 176 x 60 mm, chain 37.5 mm (might be changed if needed).',
-    price: 365
-  },
-  {
-    images: [
-      {
-        big: 'e3eeb269-d5c1-44e0-8b2d-df5e361a0bdd.webp',
-        medium: 'e3eeb269-d5c1-44e0-8b2d-df5e361a0bdd300.webp',
-        thumb: 'e3eeb269-d5c1-44e0-8b2d-df5e361a0bdd92.webp'
-      }
-    ],
-    materials: ['Ebony, silver, aquamarines.'],
-    category: 'brooch',
-    available: true,
-    name: "n'B2",
-    slug: "n'B2",
-    description:
-      'Unique design handcarved ebony statement brooch.\n\nEbony is hand carved to create layers that twist, presenting a flat surface to display the domes of silver and encased aquamarines. The silver and blue contrasts brilliantly against the black ebony.',
-    group: 'flow',
-    size: '12x1,2cm',
-    price: 340
+    madeToOrder: true,
+    producingTime: '1 to 2 weeks',
+    display: true,
+    oneOfAKind: false,
+    name: '1',
+    slug: '1',
+    description: '1',
+    materials: '',
+    group: '123',
+    size: '',
+    weight: '',
+    price: 1,
+    frontImage: 'e96dddc9-af28-479f-a067-996d83669e80300.JPG',
+    galleryIndex: 65,
+    collectionIndex: 65
   }
 ];
