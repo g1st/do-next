ece1ebd25564003b722f1c0c070c4341b7b17d98
g1st <gintstan@gmail.com>
2020-01-18 19:41:16 +0000

    show gallery on landing page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
@@ -10,49 +9,71 @@ import LandingPageGallery from '../components/LandingPageGallery/LandingPageGall
 import Newsletter from '../components/Newsletter/Newsletter';
 import InstagramGallery from '../components/InstagramGallery/InstagramGallery';
 import UpcomingEvent from '../components/UpcomingEvent/UpcomingEvent';
+import GridGallery from '../components/Gallery/Gallery';
 
 const styles = () => ({
   button: {
-    maxWidth: '300px',
+    maxWidth: '118px',
     display: 'block',
-    margin: '40px auto 100px auto',
-    padding: '10px',
-    '@media (min-width: 960px)': {
-      display: 'none'
-    }
+    margin: '60px auto 100px auto'
   }
 });
 
 const handleButton = () => {
   Router.push('/gallery');
 };
-const Index = ({ pathname, collections, classes, user }) => (
-  <Layout pathname={pathname} collections={collections} user={user}>
-    <LandingPageGallery />
-    <Button
-      variant="contained"
-      color="secondary"
-      fullWidth
-      className={classes.button}
-      onClick={handleButton}
-    >
-      GALLERY
-    </Button>
-    <UpcomingEvent />
-    <InView triggerOnce rootMargin="400px">
-      {({ inView, ref }) => (
-        <div ref={ref}>{inView ? <InstagramGallery /> : null}</div>
-      )}
-    </InView>
-    <Newsletter />
-  </Layout>
-);
+const Index = ({ pathname, collections, classes, user, data, router }) => {
+  let { collection } = router.query;
+
+  if (!collections.includes(collection)) {
+    collection = 'all';
+  }
+  return (
+    <Layout pathname={pathname} collections={collections} user={user}>
+      <LandingPageGallery />
+      <UpcomingEvent />
+      <InView triggerOnce rootMargin="300px">
+        {({ inView, ref }) => (
+          <div ref={ref}>
+            {inView ? (
+              <>
+                <GridGallery
+                  user={user}
+                  data={data.filter(item => item.group === 'silver-flow')}
+                  showCollection={collection}
+                  collectionsNames={collections}
+                />
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  className={classes.button}
+                  onClick={handleButton}
+                  size="medium"
+                >
+                  Show all
+                </Button>
+              </>
+            ) : null}
+          </div>
+        )}
+      </InView>
+      <InView triggerOnce rootMargin="100px">
+        {({ inView, ref }) => (
+          <div ref={ref}>{inView ? <InstagramGallery /> : null}</div>
+        )}
+      </InView>
+      <Newsletter />
+    </Layout>
+  );
+};
 
 Index.propTypes = {
   pathname: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
   collections: PropTypes.arrayOf(PropTypes.string),
   classes: PropTypes.object.isRequired,
-  user: PropTypes.string
+  user: PropTypes.string,
+  data: PropTypes.array,
+  router: PropTypes.object
 };
 
 Index.getInitialProps = async ({ pathname, user }) => ({ pathname, user });
