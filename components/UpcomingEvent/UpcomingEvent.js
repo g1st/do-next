883e5aa3f9e8c79a2d0fe3f37687b6ec1f06e1e4
g1st <gintstan@gmail.com>
2020-03-24 21:57:00 +0000

    Make event image accessible by tabbing

--- components/UpcomingEvent/UpcomingEvent.js
+++ components/UpcomingEvent/UpcomingEvent.js
@@ -1,19 +1,39 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import Banner from '../../static/images/Banner-1.JPG';
 import { Image, EventWrapper } from '../../styles/UpcomingEvent';
 
-const UpcomingEvent = () => (
-  <EventWrapper>
-    <Link href="/gallery?collection=silver-flow" as="/gallery/silver-flow">
-      <Image
-        src={Banner}
-        alt="Silver Flow collection banner"
-        sizes=""
-        srcSet=""
-      />
-    </Link>
-  </EventWrapper>
-);
+const UpcomingEvent = () => {
+  const router = useRouter();
+
+  const handleKeyDown = (href, as, key) => {
+    if (key === 'Enter' || key === ' ') {
+      router.push(href, as);
+    }
+  };
+
+  return (
+    <EventWrapper>
+      <Link href="/gallery?collection=silver-flow" as="/gallery/silver-flow">
+        <Image
+          src={Banner}
+          alt="Silver Flow collection banner"
+          sizes=""
+          srcSet=""
+          tabIndex="0"
+          aria-label="View Silver Flow collection"
+          onKeyDown={e =>
+            handleKeyDown(
+              '/gallery?collection=silver-flow',
+              '/gallery/silver-flow',
+              e.key
+            )
+          }
+        />
+      </Link>
+    </EventWrapper>
+  );
+};
 
 export default UpcomingEvent;
