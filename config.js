36ce4598f47ae036c0feb82485d1481a28854cae
g1st <gintstan@gmail.com>
2020-04-06 16:41:25 +0100

    Update config.

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ module.exports = {
   authUrl: 'https://auth-server.dovilejewellery.com',
   appUrl: 'https://dovilejewellery.com',
   ITEMS_PER_PAGE: 30,
-  instagramToken: '1759380932.563a814.3e0080c9dd354f678a841b71fa9de82c'
+  instagramToken: '1759380932.563a814.3e0080c9dd354f678a841b71fa9de82c',
+  awsBucket: 'https://dovileko.s3.eu-west-2.amazonaws.com'
 };
