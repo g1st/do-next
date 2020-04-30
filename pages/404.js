3e91dddbad8928354f0cb18f22ab47f6a4c71687
g1st <gintstan@gmail.com>
2020-04-30 09:26:15 +0100

    Add 404.js page for auto static optimization

---
+++ pages/404.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import Error from 'next/error';
+
+import Layout from '../components/Layout';
+
+const CustomError = ({
+  pathname = '/',
+  collections = ['flow', 'silver-flow', 'various'],
+}) => (
+  <Layout
+    collections={collections}
+    title="Jewellery artist Dovile Kondrasovaite | Dovile Jewellery"
+    description="Contemporary amber and precious metals jewellery by an independent artist Dovile Kondrasovaite. Handmade in Birmingham's historic Jewellery Quarter, UK."
+    pathname={pathname}
+  >
+    <Error statusCode={404} />
+  </Layout>
+);
+
+CustomError.propTypes = {
+  pathname: PropTypes.string,
+  collections: PropTypes.arrayOf(PropTypes.string),
+};
+
+export default CustomError;
