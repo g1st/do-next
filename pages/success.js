dcf5cb4ab096a7e4cc78e25c199bf062ac72ac2b
g1st <gintstan@gmail.com>
2020-10-14 09:23:32 +0100

    Add successful payment page

---
+++ pages/success.js
@@ -0,0 +1,86 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+import Layout from '../components/Layout';
+import { WidthContainer } from '../styles/Shared';
+import { clearBuyItNow, clearCart } from '../store/actions';
+
+const styles = () => ({
+  marginTop: {
+    marginTop: '30px',
+  },
+});
+
+const Success = function ({
+  collections,
+  classes,
+  user,
+  clearBuyItNow: clearBuyItNowRedux,
+  clearCart: clearCartRedux,
+}) {
+  const router = useRouter();
+  const sessionId = router.query.session_id;
+
+  useEffect(() => {
+    async function fetchSession() {
+      axios
+        .get(
+          `${process.env.NEXT_PUBLIC_APP_URL}/api/checkout-session?sessionId=${sessionId}`
+        )
+        .then((res) => res.data)
+        .then((session) => {
+          if (session.payment_status === 'paid') {
+            clearBuyItNowRedux();
+            clearCartRedux();
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    fetchSession();
+  }, [sessionId, clearBuyItNowRedux, clearCartRedux]);
+
+  return (
+    <Layout
+      pathname={false}
+      collections={collections}
+      title="Successful payment | Dovile Jewellery"
+      user={user}
+    >
+      <WidthContainer>
+        <Typography variant="body1" className={classes.marginTop}>
+          Your order was successful and a confirmation email was sent to your
+          provided email.
+        </Typography>
+        <Typography variant="body1" className={classes.marginTop}>
+          Please note that it can take a couple of minutes to arrive and might
+          land in your spam folder.
+        </Typography>
+        <Typography variant="body1" className={classes.marginTop}>
+          Many thanks, Dovile.
+        </Typography>
+      </WidthContainer>
+    </Layout>
+  );
+};
+
+Success.propTypes = {
+  collections: PropTypes.arrayOf(PropTypes.string),
+  classes: PropTypes.object.isRequired,
+  user: PropTypes.string,
+  clearBuyItNow: PropTypes.func,
+  clearCart: PropTypes.func,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  clearBuyItNow: () => dispatch(clearBuyItNow()),
+  clearCart: () => dispatch(clearCart()),
+});
+
+export default connect(null, mapDispatchToProps)(withStyles(styles)(Success));
