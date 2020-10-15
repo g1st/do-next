8a88abaa787565e45f61330da2907a898f2de876
g1st <gintstan@gmail.com>
2020-10-15 07:08:49 +0100

    Remove obsolete method

--- util/helpers.js
+++ util/helpers.js
@@ -70,33 +70,6 @@ exports.pluralise = (category) => {
   }
 };
 
-exports.generatePaymentResponse = (intent) => {
-  if (
-    (intent.status === 'requires_action' ||
-      intent.status === 'requires_source_action') &&
-    intent.next_action.type === 'use_stripe_sdk'
-  ) {
-    // Tell the client to handle the action
-    return {
-      requires_action: true,
-      payment_intent_client_secret: intent.client_secret,
-    };
-  }
-
-  if (intent.status === 'succeeded') {
-    // The payment didn’t need any additional actions and completed!
-    // Handle post-payment fulfillment
-    return {
-      success: true,
-    };
-  }
-
-  // Invalid status
-  return {
-    errors: [{ msg: 'Invalid PaymentIntent status', param: '_error' }],
-  };
-};
-
 exports.filterCollections = (data, user) => {
   let collections;
   if (user) {
